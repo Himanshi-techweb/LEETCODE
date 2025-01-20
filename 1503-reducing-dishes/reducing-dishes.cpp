2e0408f93636d5092b8749f479142ac5e313afc5
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-01-20 22:59:55 +0530

    Time: 16 ms (36.93%) | Memory: 24.9 MB (38.09%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.cpp
+++ 1503-reducing-dishes/reducing-dishes.cpp
@@ -8,17 +8,30 @@ public:
     //    dp[i][time]=max(include,exclude);
     //    return dp[i][time]; 
     // }
+    // int solveTab(vector<int> &arr){
+    //     int n=arr.size();
+    //     vector<vector<int>>dp(n+1,vector<int> (n+1,0));
+    //     for(int i=n-1;i>=0;i--){
+    //         for(int time=n-1;time>=0;time--){
+    //             int include=arr[i]*(time+1)+dp[i+1][time+1];
+    //             int exclude=dp[i+1][time];
+    //             dp[i][time]=max(include,exclude);
+    //         }
+    //     }
+    //     return dp[0][0];
+    // }
     int solveTab(vector<int> &arr){
         int n=arr.size();
-        vector<vector<int>>dp(n+1,vector<int> (n+1,0));
+        vector<int> curr(n+1,0);
+        vector<int> next(n+1,0);
         for(int i=n-1;i>=0;i--){
             for(int time=n-1;time>=0;time--){
-                int include=arr[i]*(time+1)+dp[i+1][time+1];
-                int exclude=dp[i+1][time];
-                dp[i][time]=max(include,exclude);
-            }
+                int include=arr[i]*(time+1)+next[time+1];
+                int exclude=next[time];
+                curr[time]=max(include,exclude);
+            }next=curr;
         }
-        return dp[0][0];
+        return next[0];
     }
     int maxSatisfaction(vector<int>& arr) {
       vector<vector<int>> dp(arr.size(),vector<int>(arr.size(),-1));
