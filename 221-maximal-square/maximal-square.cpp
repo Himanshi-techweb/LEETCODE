9ee325f5e1679beeb8cffe406602bdab29d42552
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-01-20 15:32:19 +0530

    Time: 3 ms (90.88%) | Memory: 30.9 MB (53.39%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -1,27 +1,52 @@
 class Solution {
 public:
-    int solve(vector<vector<char>>&matrix,int i,int j,int &maxi,vector<vector<int>> &dp){
-        if(i>=matrix.size() || j>=matrix[0].size())return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        int right=solve(matrix,i,j+1,maxi,dp);
-        int down=solve(matrix,i+1,j,maxi,dp);
-        int diagonal=solve(matrix,i+1,j+1,maxi,dp);
-        if(matrix[i][j]=='1'){
-            dp[i][j]=1+min(right,min(down,diagonal));
-            maxi=max(maxi,dp[i][j]);
-            return dp[i][j];
+    // int solve(vector<vector<char>>&matrix,int i,int j,int &maxi,vector<vector<int>> &dp){
+    //     if(i>=matrix.size() || j>=matrix[0].size())return 0;
+    //     if(dp[i][j]!=-1)return dp[i][j];
+    //     int right=solve(matrix,i,j+1,maxi,dp);
+    //     int down=solve(matrix,i+1,j,maxi,dp);
+    //     int diagonal=solve(matrix,i+1,j+1,maxi,dp);
+    //     if(matrix[i][j]=='1'){
+    //         dp[i][j]=1+min(right,min(down,diagonal));
+    //         maxi=max(maxi,dp[i][j]);
+    //         return dp[i][j];
+    //     }
+    //     else{
+    //         return dp[i][j]=0;
+    //     }
+        
+    // }
+    int solveTab(vector<vector<char>>&matrix,int &maxi){
+        int m=matrix.size();
+        int n=matrix[0].size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        // if(i>=matrix.size() || j>=matrix[0].size())return dp[i][j]=0;
+        // if(dp[i][j]!=-1)return dp[i][j];
+        for(int i=m-1;i>=0;i--){
+          for(int j=n-1;j>=0;j--){
+            int right=dp[i][j+1];
+            int down=dp[i+1][j];
+            int diagonal=dp[i+1][j+1];
+            if(matrix[i][j]=='1'){
+              dp[i][j]=1+min(right,min(down,diagonal));
+              maxi=max(maxi,dp[i][j]);
+            }
+            else{
+              dp[i][j]=0;
+            }
+          }
+
         }
-        else{
-            return dp[i][j]=0;
-        }
+        return dp[1][1];
+        
         
     }
     int maximalSquare(vector<vector<char>>& matrix) {
     int m=matrix.size();
     int n=matrix[0].size() ;
     int maxi=0;
-    vector<vector<int>> dp(m,vector<int>(n,-1));
-    solve(matrix,0,0,maxi,dp); 
+    // vector<vector<int>> dp(m,vector<int>(n,-1));
+    solveTab(matrix,maxi); 
     return maxi*maxi;   
     }
 };
