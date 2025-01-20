14499b44cd4e605b9bbe10fde2015b13dcfcc9db
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-01-20 16:35:54 +0530

    Time: 3 ms (61.48%) | Memory: 11.4 MB (15.06%) - LeetSync

--- 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int solve(vector<int> &arr,int i,int j,vector<vector<int>> &dp){
+    int solve(vector<int> &arr,int i,int j,vector<vector<int>> &dp,int &ans){
         if(i+1==j)return 0;
         if(dp[i][j]!=-1)return dp[i][j];
         int minscore=INT_MAX;
         for(int k=i+1;k<j;k++){
-          dp[i][j]=min(minscore,arr[i]*arr[j]*arr[k]+solve(arr,i,k,dp)+solve(arr,k,j,dp));
+          dp[i][j]=min(minscore,arr[i]*arr[j]*arr[k]+solve(arr,i,k,dp,ans)+solve(arr,k,j,dp,ans));
           minscore=min(dp[i][j],minscore);
+          ans=minscore;
         }
         return minscore;
     }
     int minScoreTriangulation(vector<int>& values) {
         int x=values.size();
+        int ans=INT_MAX;
         vector<vector<int>>dp(x,vector<int>(x,-1));
-       int ans=solve(values,0,values.size()-1,dp);
+       solve(values,0,values.size()-1,dp,ans);
        return ans; 
     }
 };
