4e5650cc624da424a617c515078f9a1bda71142a
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-01-22 14:33:24 +0530

    Time: 7 ms (87.32%) | Memory: 12.5 MB (87.32%) - LeetSync

--- 1489-pizza-with-3n-slices/pizza-with-3n-slices.cpp
+++ 1489-pizza-with-3n-slices/pizza-with-3n-slices.cpp
@@ -20,27 +20,35 @@ public:
         int n=arr.size();
         int include=0;
         int exclude=0;
-        vector<vector<int>> dp1(n+2,vector<int> (n+2,0));
-        vector<vector<int>> dp2(n+2,vector<int> (n+2,0));
+        // vector<vector<int>> dp1(n+2,vector<int> (n+2,0));
+        // vector<vector<int>> dp2(n+2,vector<int> (n+2,0));
+        vector<int>prev1(n+2,0);
+        vector<int>prev2(n+2,0);
+        vector<int>curr(n+2,0);
         for(int i=n-1;i>=1;i--){
             for(int k=1;k<=n/3;k++){
-                include=arr[i]+dp1[i+2][k-1];
-                exclude=dp1[i+1][k];
-                dp1[i][k]=max(include,exclude);
+                include=arr[i]+prev2[k-1];
+                exclude=prev1[k];
+                curr[k]=max(include,exclude);
             }
-            
+            prev2=prev1;
+            prev1=curr;
         }
-        int case1= dp1[1][n/3];
+        int case1= prev1[n/3];
 
-
+        vector<int>prev3(n+2,0);
+        vector<int>prev4(n+2,0);
+        vector<int>curr2(n+2,0);
         for(int i=n-2;i>=0;i--){
             for(int k=1;k<=n/3;k++){
-                include=arr[i]+dp2[i+2][k-1];
-                exclude=dp2[i+1][k];
-                dp2[i][k]=max(include,exclude);
+                include=arr[i]+prev4[k-1];
+                exclude=prev3[k];
+                curr2[k]=max(include,exclude);
             } 
+            prev4=prev3;
+            prev3=curr2;
         }
-        int case2= dp2[0][n/3];
+        int case2= prev3[n/3];
         return max(case1,case2);
     }
     int maxSizeSlices(vector<int>& slices) {
