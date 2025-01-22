11be31d712b6711532b906ebc192f6a58c192e71
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-01-22 18:41:46 +0530

    Time: 135 ms (43.35%) | Memory: 16.9 MB (73.62%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -8,13 +8,31 @@ public:
     //     return include || exclude; 
     //    } 
 
-    bool solveMem(int i,vector<int> &arr,int tar,vector<vector<int>>&dp){
-        if(i>=arr.size() || tar<0)return 0;
-        if(tar==0)return 1;
-        if(dp[i][tar]!=-1)return dp[i][tar];
-        bool include=solveMem(i+1,arr,tar-arr[i],dp);
-        bool exclude=solveMem(i+1,arr,tar,dp);
-        return dp[i][tar]= include || exclude; 
+    // bool solveMem(int i,vector<int> &arr,int tar,vector<vector<int>>&dp){
+    //     if(i>=arr.size() || tar<0)return 0;
+    //     if(tar==0)return 1;
+    //     if(dp[i][tar]!=-1)return dp[i][tar];
+    //     bool include=solveMem(i+1,arr,tar-arr[i],dp);
+    //     bool exclude=solveMem(i+1,arr,tar,dp);
+    //     return dp[i][tar]= include || exclude; 
+    // } 
+
+    bool solveTab(vector<int> &arr,int tar){
+        vector<vector<bool>>dp(arr.size()+1,vector<bool>(tar+1,false));
+        for(int i=0;i<arr.size();i++){
+            dp[i][0]=true;
+        }
+        for(int i=arr.size()-1;i>=0;i--){
+            for(int j=1;j<=tar;j++){
+              if((j-arr[i])>=0){
+                dp[i][j]=dp[i+1][j-arr[i]] || dp[i+1][j];
+              }
+              else{
+                dp[i][j]=dp[i+1][j];
+              }
+            }
+        }
+        return dp[0][tar];
     } 
 
        bool canPartition(vector<int>& nums) {
@@ -25,7 +43,8 @@ public:
        if(target%2!=0)return 0;
         target=target/2;
         // return solve(0,nums,target);
-        vector<vector<int>> dp(nums.size(),vector<int>(target+1,-1));
-        return solveMem(0,nums,target,dp);
+        // vector<vector<int>> dp(nums.size(),vector<int>(target+1,-1));
+        // return solveMem(0,nums,target,dp);
+        return solveTab(nums,target);
     }
 };
