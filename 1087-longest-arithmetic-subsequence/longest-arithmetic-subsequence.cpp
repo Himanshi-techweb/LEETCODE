494335a776fd6fe769352c4449d5cf4ee89c7c1e
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-01-23 15:55:29 +0530

    Time: 3060 ms (5.04%) | Memory: 242.4 MB (59.12%) - LeetSync

--- 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
@@ -25,7 +25,7 @@ public:
     //    }
     //    return ans;
     // }                                                                                           
-    int solve(int i,vector<int> & arr,int diff,unordered_map<int,int> dp[]){
+    int solve(int i,vector<int> & arr,int diff,vector<unordered_map<int,int>> &dp){
         if(i<0)return 0;
         if(dp[i].count(diff))return dp[i][diff];
         int ans=0;
@@ -40,7 +40,8 @@ public:
     int longestArithSeqLength(vector<int>& nums) {
       int n=nums.size();
       int ans=0;
-      unordered_map<int,int> dp[n+1];
+    //   unordered_map<int,int> dp[n+1];
+    vector<unordered_map<int,int>> dp(n+1);
       for(int i=0;i<n-1;i++){
         for(int j=i+1;j<n;j++){
             ans=max(ans,2+solve(i,nums,nums[j]-nums[i],dp));
