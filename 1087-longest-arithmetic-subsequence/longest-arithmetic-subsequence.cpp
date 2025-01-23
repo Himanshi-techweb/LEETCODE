dcc1a9838c8ba8a90afca0a403a6dcc3542f67f8
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-01-23 16:09:43 +0530

    Time: 1185 ms (48.35%) | Memory: 248.3 MB (29.43%) - LeetSync

--- 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
@@ -24,30 +24,41 @@ public:
     //     }
     //    }
     //    return ans;
-    // }                                                                                           
-    int solve(int i,vector<int> & arr,int diff,vector<unordered_map<int,int>> &dp){
-        if(i<0)return 0;
-        if(dp[i].count(diff))return dp[i][diff];
-        int ans=0;
+    // // }                                                                                           
+    // int solveMem(int i,vector<int> & arr,int diff,vector<unordered_map<int,int>> &dp){
+    //     if(i<0)return 0;
+    //     if(dp[i].count(diff))return dp[i][diff];
+    //     int ans=0;
 
-        for(int k=i-1;k>=0;k--){
-            if(arr[i]-arr[k]==diff){
-                ans=max(ans,1+solve(k,arr,diff,dp));
-            }
-        }
-       return dp[i][diff]=ans;
-    }                                                                                           
+    //     for(int k=i-1;k>=0;k--){
+    //         if(arr[i]-arr[k]==diff){
+    //             ans=max(ans,1+solve(k,arr,diff,dp));
+    //         }
+    //     }
+    //    return dp[i][diff]=ans;
+    // }                                                                                           
     int longestArithSeqLength(vector<int>& nums) {
       int n=nums.size();
       if(n<=2)return n;
       int ans=0;
-    //   unordered_map<int,int> dp[n+1];
+    // //   unordered_map<int,int> dp[n+1];
     vector<unordered_map<int,int>> dp(n+1);
-      for(int i=0;i<n-1;i++){
-        for(int j=i+1;j<n;j++){
-            ans=max(ans,2+solve(i,nums,nums[j]-nums[i],dp));
+    //   for(int i=0;i<n-1;i++){
+    //     for(int j=i+1;j<n;j++){
+    //         ans=max(ans,2+solve(i,nums,nums[j]-nums[i],dp));
+    //     }
+    //   } 
+    //   return ans;
+    for(int i=1;i<n;i++){
+        for(int j=0;j<i;j++){
+            int diff=nums[i]-nums[j];
+            int count=1;
+            if(dp[j].count(diff)){
+                count=dp[j][diff];
+            }
+            dp[i][diff]=1+count;
+            ans=max(ans,dp[i][diff]);
         }
-      } 
-      return ans;
+    }return ans;
     }
 };
