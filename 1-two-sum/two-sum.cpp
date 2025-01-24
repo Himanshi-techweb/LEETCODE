618a00bbadce0a5ae2706182a905c99977f46422
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-01-24 13:12:03 +0530

    Time: 3 ms (74.95%) | Memory: 14.9 MB (20.85%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,18 +4,18 @@ public:
       vector<int> ans;
       int i=0;
       unordered_map<int,int> check;
-      for(int i=0;i<arr.size();i++){
-          check[arr[i]]=i;
-      }
+    //   for(int i=0;i<arr.size();i++){
+    //       check[arr[i]]=i;
+    //   }
       for(int i=0;i<arr.size();i++){
         int temp=target-arr[i];
         if(check.find(temp)!=check.end() && check[temp]!=i){
-            ans.push_back(check[temp]);
-            ans.push_back(i);
-            break;
+            // ans.push_back(check[temp]);
+            // ans.push_back(i);
+            return {check[temp],i};
         }
-        
+        check.insert({arr[i],i});
       }
-      return ans;
+      return {};
     }
 };
