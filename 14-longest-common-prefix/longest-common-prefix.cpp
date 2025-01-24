8b1f5bab04dfeb0e22af5df6b05e43d8b8586981
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-01-24 22:46:36 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (84.21%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -14,21 +14,31 @@ public:
             i++;
         }return ans;
     }
-    string longestCommonPrefix(vector<string>& strs) {
-      string x;
-      stack<string> check;
-      for(auto x:strs){
-          check.push(x);
-      } 
-      while(check.size()!=1){
-        string temp1=check.top();
-        check.pop();
-        string temp2=check.top();
-        check.pop();
-        string ans=common(temp1,temp2);
-        if(ans=="")return "";
-        check.push(ans);
-      }
-      return check.top();
+    string longestCommonPrefix(vector<string>& str) {
+    //   string x;
+    //   stack<string> check;
+    //   for(auto x:strs){
+    //       check.push(x);
+    //   } 
+    //   while(check.size()!=1){
+    //     string temp1=check.top();
+    //     check.pop();
+    //     string temp2=check.top();
+    //     check.pop();
+    //     string ans=common(temp1,temp2);
+    //     if(ans=="")return "";
+    //     check.push(ans);
+    //   }
+    //   return check.top();
+    string prefix=str[0];
+    for(int i=1;i<str.size();i++){
+        int j=0;
+        while(j<prefix.length() && j<str[i].length() && prefix[j]==str[i][j]){
+            j++;
+        }
+        prefix=prefix.substr(0,j);
+        if(prefix=="")return "";
+    }
+    return prefix;
     }
 };
