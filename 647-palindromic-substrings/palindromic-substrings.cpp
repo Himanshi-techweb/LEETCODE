cf3c290be178364d18ca055c72f5159775cb560d
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-01-25 19:14:41 +0530

    Time: 7 ms (69.25%) | Memory: 8.6 MB (69.47%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+       int x=s.length();
+       int max_len=0;
+       int count=0;
+            auto check=[&](int l,int r){
+               while(l>=0 && r<x && s[l]==s[r]){
+              if(r-l+1>max_len){
+                max_len=max(max_len,r-l+1);
+                // ans=s.substr(l,max_len);  
+              }
+              count++;
+              l--;
+              r++;
+              } 
+            }; 
+            for(int i=0;i<x;i++){
+                check(i,i);
+                check(i,i+1);
+            }return count; 
+    }
+};
