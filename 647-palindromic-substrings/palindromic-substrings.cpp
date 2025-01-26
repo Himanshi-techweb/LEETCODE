4d815f00791f60bb7818a2a30da949c8f6753aab
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-01-26 15:48:20 +0530

    Time: 7 ms (69.21%) | Memory: 8.6 MB (69.57%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -6,10 +6,10 @@ public:
        int count=0;
             auto check=[&](int l,int r){
                while(l>=0 && r<x && s[l]==s[r]){
-              if(r-l+1>max_len){
-                max_len=max(max_len,r-l+1);
-                // ans=s.substr(l,max_len);  
-              }
+            //   if(r-l+1>max_len){
+            //     max_len=max(max_len,r-l+1);
+            //     // ans=s.substr(l,max_len);  
+            //   }
               count++;
               l--;
               r++;
