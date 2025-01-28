db4e5a8e863f8500d1f06c4a1a0e7623ab0a2329
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-01-28 14:54:00 +0530

    Time: 96 ms (5.91%) | Memory: 17.6 MB (29.03%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//////////////////recurrsion
     int solve(int index,int buy,vector<int> &arr,vector<vector<int>> &dp){
         if(index==arr.size()){
             return 0;
@@ -14,23 +15,58 @@ public:
         }
         return dp[index][buy]=profit;
     }
+
+
+
+
+    /////////////tabulation
+    // int maxProfit(vector<int>& prices) {
+    //     // vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
+    //     vector<vector<int>> dp(prices.size()+1,vector<int>(2,-1));
+    //     dp[prices.size()][0]=dp[prices.size()][1]=0;
+    //     for(int index=prices.size()-1;index>=0;index--){
+    //         for(int buy=0;buy<=1;buy++){
+    //             long long profit=0;
+    //             if(buy){
+    //                profit=max(-prices[index]+dp[index+1][0],0+dp[index+1][1]);
+    //             }
+    //             else{
+    //                profit=max(prices[index]+dp[index+1][1],0+dp[index+1][0]);
+    //             }
+    //             dp[index][buy]=profit;
+    //         }
+    //     }
+    //     return dp[0][1];
+    // //    return solve(0,1,prices,dp); 
+    // }
+
+
+
+
+
+
+    /////////space optimization
     int maxProfit(vector<int>& prices) {
-        // vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
-        vector<vector<int>> dp(prices.size()+1,vector<int>(2,-1));
-        dp[prices.size()][0]=dp[prices.size()][1]=0;
+        // vector<vector<int>> dp(prices.size(),vector<int>(2,0));
+        // vector<vector<int>> dp(prices.size()+1,vector<int>(2,0));
+        vector<int> curr(prices.size()+1,0);
+        vector<int> next(prices.size()+1,0);
+
+        // dp[prices.size()][0]=dp[prices.size()][1]=0;
         for(int index=prices.size()-1;index>=0;index--){
             for(int buy=0;buy<=1;buy++){
                 long long profit=0;
                 if(buy){
-                   profit=max(-prices[index]+dp[index+1][0],0+dp[index+1][1]);
+                   profit=max(-prices[index]+next[0],0+next[1]);
                 }
                 else{
-                   profit=max(prices[index]+dp[index+1][1],0+dp[index+1][0]);
+                   profit=max(prices[index]+next[1],0+next[0]);
                 }
-                dp[index][buy]=profit;
+                curr[buy]=profit;
             }
+            next=curr;
         }
-        return dp[0][1];
+        return next[1];
     //    return solve(0,1,prices,dp); 
     }
 };
