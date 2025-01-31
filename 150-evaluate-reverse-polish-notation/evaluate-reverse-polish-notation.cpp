c217ec29a8e33ffafc55c82a139d5055a6ca0cbd
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-01-31 15:53:10 +0530

    Time: 4 ms (24.02%) | Memory: 17.1 MB (37.27%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -2,36 +2,36 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
       stack<int> q;
-      string temp=tokens[0];
-      q.push(stoi(temp));
+    //   string temp=tokens[0];
+      q.push(stoi(tokens[0]));
     //   if(temp!="+" && temp!="-" && temp!="/" && temp!="*"){
     //         int x=stoi(temp);
     //        q.push(x);
     //   }
       int i=1;
       while( i<tokens.size()){
-         temp=tokens[i];
-        if(temp!="+" && temp!="-" && temp!="/"  && temp!="*"){
-           q.push(stoi(temp));
+        //  temp=tokens[i];
+        if(tokens[i]!="+" && tokens[i]!="-" && tokens[i]!="/"  && tokens[i]!="*"){
+           q.push(stoi(tokens[i]));
         }
         else{
            int check1=q.top();
            q.pop();
            int check2=q.top();
            q.pop();
-           if(temp=="+"){
+           if(tokens[i]=="+"){
             q.push(check1+check2);
            }
-           if(temp=="-"){
+           if(tokens[i]=="-"){
             q.push(check2-check1);
            }
-           if(temp=="*"){
+           if(tokens[i]=="*"){
             q.push(check1*check2);
            }
-           if(temp=="/"){
-            if(check1!=0){
+           if(tokens[i]=="/"){
+            // if(check1!=0){
               q.push(check2/check1);
-            }  
+            // }  
            }
         }
         i++;
