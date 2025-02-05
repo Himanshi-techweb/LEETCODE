9cb799cb6279e8d78e2dabb658a36d9b9a983627
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-02-05 23:16:15 +0530

    Time: 3 ms (31.20%) | Memory: 16.7 MB (82.47%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,30 +1,14 @@
 class Solution {
 public:
-    void bfs(int r,int c, vector<vector<int>>&arr){
-       int m=arr.size();
-       int n=arr[0].size();  
-      queue<pair<int,int>> q;
-      q.push({r,c});
-     
-      while(!q.empty()){
-        int frow=q.front().first;
-        int fcol=q.front().second;
-        arr[frow][fcol]=-1;
-        q.pop();
-        
-      }
-    }
     int orangesRotting(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
         int count =0;
         int fresh=0;
-        vector<vector<int>> check;
+        vector<pair<int,int>> check;
         check={{0,-1},{0,1},{-1,0},{1,0}};
         queue<pair<int,int>> q;
         
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
                if(grid[i][j]==2){
                  q.push({i,j});
                }
@@ -42,9 +26,9 @@ public:
                 int fcol=q.front().second;
                 q.pop();
                 for(auto x: check){
-                   int nrow=x[0]+frow;
-                   int ncol=x[1]+fcol;
-                   if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol]==1){
+                   int nrow=x.first+frow;
+                   int ncol=x.second+fcol;
+                   if(nrow>=0 && nrow<grid.size() && ncol>=0 && ncol<grid[0].size() && grid[nrow][ncol]==1){
                         q.push({nrow,ncol});
                         grid[nrow][ncol]=2;
                         rotten=true;
@@ -53,7 +37,6 @@ public:
                 }
             }if(rotten)count++;
         }
-        // if(count==0)return -1;
         if(fresh!=0)return -1;
         return count;
     }
