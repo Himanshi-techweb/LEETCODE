7c617ebd0365f354ff72772caa927db95e61afde
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-02-08 18:58:31 +0530

    Time: 7 ms (93.48%) | Memory: 34.8 MB (67.03%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-      int m=mat.size();
-      int n=mat[0].size();
-      queue<pair<int,int>> q;
-
-    //   vector<vector<int>> vis(m,vector<int> (n,0));
-      vector<vector<int>> ans(m,vector<int> (n,-1));
-      for(int i=0;i<m;i++){
+       int m=mat.size();
+       int n=mat[0].size();
+       vector<vector<int>> ans(m,vector<int> (n,-1));
+       queue<pair<int,int>> q;
+       for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             if(mat[i][j]==0){
-               q.push({i,j});
-               ans[i][j]=0;
+                q.push({i,j});
+                ans[i][j]=0;
             }
         }
-      }
-      vector<int> row={-1,0,1,0};
-      vector<int> col={0,1,0,-1}; 
-      while(!q.empty()){
-        int r=q.front().first;
-        int c=q.front().second;
-        // int x=q.front().second;
-        q.pop();
-        // ans[r][c]=x;
-        for(int i=0;i<4;i++){
-            int nc=c+col[i];
-            int nr=r+row[i];
-            if(nc>=0 && nc<n && nr>=0 && nr<m && ans[nr][nc]==-1){
-                ans[nr][nc]=ans[r][c]+1;
-                q.push({nr,nc});
-            }
-        }
-      }
-      return ans;
+       }
+    //    vector<pair<int,int>> check;
+    vector<int> rowcheck={0,-1,0,1};
+    vector<int> colcheck={-1,0,1,0};
+    //    check={{0,-1},{-1,0},{0,1},{1,0}};
+       while(!q.empty()){
+          int row=q.front().first;
+          int col=q.front().second;
+          q.pop();
+          for(int i=0;i<4;i++){
+             int nrow=row+rowcheck[i];
+             int ncol=col+colcheck[i];
+             if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && ans[nrow][ncol]==-1){
+                ans[nrow][ncol]=1+ans[row][col];
+                q.push({nrow,ncol});
+             }
+          }
+       } 
+       return ans;
     }
 };
