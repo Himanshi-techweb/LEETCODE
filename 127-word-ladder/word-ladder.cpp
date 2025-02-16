4b67989707bd8fdf61b43d691df8ab3f84637312
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-02-16 12:12:58 +0530

    Time: 348 ms (28.81%) | Memory: 21.2 MB (61.66%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    bool checkfunc(string str,set<string> &check,vector<char> &alpha,queue<pair<string,int>> & q,string end,int freq){ 
-      string copy=str;
-      bool occur=false;
-      for(int i=0;i<str.size();i++){
-        for(int j=0;j<alpha.size();j++){
-            str=copy;
-            str[i]=alpha[j];
-            if(check.find(str)!=check.end()){
-              if(str==end){
-                return true;
-              }
-              check.erase(str);
-              q.push({str,freq+1});
-            }
-        }
-      }
-      return false;  
-    }
+    // bool checkfunc(string str,set<string> &check,vector<char> &alpha,queue<pair<string,int>> & q,string end,int freq){ 
+    //   string copy=str;
+    //   bool occur=false;
+    //   for(int i=0;i<str.size();i++){
+    //     for(int j=0;j<alpha.size();j++){
+    //         str=copy;
+    //         str[i]=alpha[j];
+    //         if(check.find(str)!=check.end()){
+    //           if(str==end){
+    //             return true;
+    //           }
+    //           check.erase(str);
+    //           q.push({str,freq+1});
+    //         }
+    //     }
+    //   }
+    //   return false;  
+    // }
     int ladderLength(string beginWord, string endWord, vector<string>& arr) {
       vector<char> alpha={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
       set<string> check;
@@ -33,9 +33,23 @@ public:
         string str=q.front().first;
         int freq=q.front().second;
         q.pop();
-        if(checkfunc(str,check,alpha,q,endWord,freq)==true){
-            return freq+1;
+        // if(checkfunc(str,check,alpha,q,endWord,freq)==true){
+        //     return freq+1;
+        // 
+        string str1=str;
+        for(int i=0;i<str.size();i++){
+        for(int j=0;j<alpha.size();j++){
+            str=str1;
+            str[i]=alpha[j];
+            if(check.find(str)!=check.end()){
+              if(str==endWord){
+                return freq+1;
+              }
+              check.erase(str);
+              q.push({str,freq+1});
+            }
         }
+      }
       }  
       return 0;
     }
