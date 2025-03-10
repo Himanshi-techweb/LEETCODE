8731ddc35a3ec7f600728fe4202a4b1bacc75e78
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-03-10 19:46:33 +0530

    Time: 68 ms (25.09%) | Memory: 21 MB (48.55%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -24,10 +24,49 @@ public:
         }
         return dp[i][j1][j2]=check;
     }
+    // int cherryPickup(vector<vector<int>>& grid) {
+    //    int m=grid.size();
+    //    int n=grid[0].size(); 
+    //    vector<vector<vector<int>>> matrix(m,vector<vector<int>> (n,vector<int>(n,-1)));
+    //    return solve(0,0,n-1,m,n,grid,matrix);
+    // }
     int cherryPickup(vector<vector<int>>& grid) {
        int m=grid.size();
        int n=grid[0].size(); 
-       vector<vector<vector<int>>> matrix(m,vector<vector<int>> (n,vector<int>(n,-1)));
-       return solve(0,0,n-1,m,n,grid,matrix);
+       vector<vector<vector<int>>> dp(m,vector<vector<int>> (n,vector<int>(n,0)));
+       for(int j1=0;j1<n;j1++){
+        for(int j2=0;j2<n;j2++){
+            if(j1==j2)dp[m-1][j1][j2]=grid[m-1][j1];
+            else dp[m-1][j1][j2]=grid[m-1][j1]+grid[m-1][j2];
+        }
+       }
+       for(int i=m-2;i>=0;i--){
+        for(int j1=0;j1<n;j1++){
+            for(int j2=0;j2<n;j2++){
+                int check=-1e8;
+                for(int q=-1;q<=1;q++){
+                for(int r=-1;r<=1;r++){
+                  int x=0;
+                  if(j1==j2){
+                    x=grid[i][j1];
+                  } 
+                  else{
+                    x=grid[i][j2]+grid[i][j1];
+                  }
+            //    x+=solve(i+1,j1+q,j2+r,m,n,arr,dp);
+                 if(j1+q>=0 && j1+q<n && j2+r>=0 && j2+r<n ){
+                   x+=dp[i+1][j1+q][j2+r];
+                 } 
+                 else {
+                   x+=-1e8;
+                 }
+                 check=max(check,x);
+            }
+            }
+            dp[i][j1][j2]=check; 
+            }
+        }
+       }
+       return dp[0][0][n-1];
     }
 };
