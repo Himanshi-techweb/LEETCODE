cc0100c783d56ffd4dfa1bb45fe91402645dbd60
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-03-11 15:34:43 +0530

    Time: 31 ms (97.11%) | Memory: 45.6 MB (43.74%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,30 +1,23 @@
 class Solution {
-    //   int sum=0;
-    //   int x=0;
-    //   for(int i=0;i<arr.size();i++){
-    //     if(sum<k){
-    //         sum+=arr[i];
-    //     }
-    //     if(sum>k){
-    //         sum-=arr[i];
-    //     }
-    //     else{
-    //         x++;
-    //     }
-    //   } return x-1; 
 public:
-    int subarraySum(vector<int>& arr, int k) {
-      unordered_map<int,int> check;
-      int count=0;
-      int sum=0;
-      check[0]=1;
-      for(auto x:arr){
-         sum+=x;
-        if(check.find(sum-k)!=check.end()){
-            count+=check[sum-k];
-        }
-        check[sum]++;
-      }
-      return count++;
+    // int solve(int i,vector<int>&arr,int k){
+    //     if(i>=arr.size())return 0;
+    //     if()
+    // }
+    int subarraySum(vector<int>& nums, int k) {
+       //vector<int> check(nums.size());
+       unordered_map<int,int> check;
+       check[0]=1;
+       int j=0;
+       int sum=0;
+       int ans=0;
+       while( j<nums.size()){
+         sum+=nums[j];
+         if(check.find(sum-k)!=check.end()){
+            ans+=check[sum-k];
+         }
+         check[sum]++;
+         j++;
+       } return ans;
     }
 };
