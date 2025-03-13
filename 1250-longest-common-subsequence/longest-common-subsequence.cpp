47380643bfaab393cbe6264573677bdbcc9284fe
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-03-13 22:30:53 +0530

    Time: 24 ms (70.46%) | Memory: 27.4 MB (50.58%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -11,8 +11,6 @@ public:
     //    //not match
     //    int notmatch=max(solve(i1,i2-1,text1,text2,dp),solve(i1-1,i2,text1,text2,dp));
     //    return dp[i1][i2]=max(match,notmatch);
-
-
        if(text1[i1]==text2[i2]){
         return dp[i1][i2]=1+solve(i1-1,i2-1,text1,text2,dp);
        }
@@ -22,7 +20,22 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
        int n1=text1.size();
        int n2=text2.size();
-       vector<vector<int> >dp(n1,vector<int> (n2,-1));
-       return solve(n1-1,n2-1,text1,text2,dp); 
+       vector<vector<int> >dp(n1+1,vector<int> (n2+1,0));
+       //return solve(n1-1,n2-1,text1,text2,dp);
+      for(int i=0;i<=n2;i++){
+        dp[0][i]=0;
+      }
+      for(int i=0;i<=n1;i++){
+        dp[i][0]=0;
+      } 
+      for(int i=1;i<=n1;i++){
+        for(int j=1;j<=n2;j++){
+           if(text1[i-1]==text2[j-1]){
+           dp[i][j]=1+dp[i-1][j-1];
+           }
+           else dp[i][j]=max(dp[i][j-1],dp[i-1][j]); 
+        }
+      } 
+      return dp[n1][n2];
     }
 };
