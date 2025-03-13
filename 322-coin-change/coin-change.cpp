5d3f532d487586edf9be8cbe8f4e2d91608e3d5a
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-03-13 15:48:35 +0530

    Time: 40 ms (41.84%) | Memory: 48.9 MB (36.37%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -13,10 +13,27 @@ public:
         int nottake=solve(i-1,a,arr,dp);
         return dp[i][a]=min(take,nottake);
     }
-    int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-        vector<vector<int>> dp(n,vector<int> (amount+1,-1));
-        int ans= solve(n-1,amount,coins,dp);
-        return (ans>=1e9)?-1:ans;
+    int coinChange(vector<int>& arr, int amount) {
+        int n=arr.size();
+        vector<vector<int>> dp(n,vector<int> (amount+1,1e9));
+        // int ans= solve(n-1,amount,coins,dp);
+        // return (ans>=1e9)?-1:ans;
+        
+        dp[0][0]=0;
+        for(int i=arr[0];i<=amount;i++){
+            if(i%arr[0]==0)dp[0][i]=i/arr[0];
+            else dp[0][i]=1e9;
+        }
+        
+        for(int i=1;i<n;i++){
+            for(int a=0;a<=amount;a++){
+                int take=1e9;
+                if(arr[i]<=a)take=1+dp[i][a-arr[i]];
+                int nottake=dp[i-1][a];
+                dp[i][a]=min(take,nottake);
+            }
+        }
+         int ans= dp[n-1][amount];
+         return (ans>=1e9)?-1:ans;
     }
 };
