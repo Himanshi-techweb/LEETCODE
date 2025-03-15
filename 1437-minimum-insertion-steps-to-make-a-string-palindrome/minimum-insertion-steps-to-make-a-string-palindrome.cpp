2f89924a619bdb4db7a202df1dcd0d8b9b77ce03
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-03-15 12:29:45 +0530

    Time: 19 ms (88.81%) | Memory: 9.7 MB (95.07%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -5,22 +5,26 @@ public:
       reverse(s.begin(),s.end());
       string t2=s;
       int k=s.size();
-      vector<vector<int>>dp(k+1,vector<int> (k+1,0));
-      for(int i=0;i<=k;i++){
-        dp[0][i]=0;
-        dp[i][0]=0;
-      }
+    //   vector<vector<int>>dp(k+1,vector<int> (k+1,0));
+    vector<int> prev(k+1,0);
+    vector<int> curr(k+1,0);
+    //   for(int i=0;i<=k;i++){
+    //     dp[0][i]=0;
+    //     dp[i][0]=0;
+    //   }
+    prev[0]=curr[0]=0;
       for(int i=1;i<=k;i++){
         for(int j=1;j<=k;j++){
             if(t1[i-1]==t2[j-1]){
-                dp[i][j]=1+dp[i-1][j-1];
+                curr[j]=1+prev[j-1];
             }
             else{
-                dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+                curr[j]=max(curr[j-1],prev[j]);
             }
         }
+        prev=curr;
       } 
-      return k-dp[k][k];
+      return k-prev[k];
     }
 
 };
