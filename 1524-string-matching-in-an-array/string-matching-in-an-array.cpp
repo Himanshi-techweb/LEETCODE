68ae3c8708d723d22b47190c3251445e07ca9a1c
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-03-16 19:09:13 +0530

    Time: 3 ms (48.98%) | Memory: 11.4 MB (78.58%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     vector<string> stringMatching(vector<string>& words) {
-     set<string> q;
+     //set<string> q;
+     vector<string> ans;
      for(int i=0;i<words.size();i++){
-        string str1=words[i];
         for(int j=0;j<words.size();j++){
-            if(j==i)continue;
-            string str2=words[j];
-            if(str1.size()<=str2.size()){
-                if(str2.find(str1)!=string::npos){
-                    q.insert(str1);
-                }
+            //if(j==i)continue;
+            if(i!=j && words[j].find(words[i])!=string::npos){
+                ans.push_back(words[i]);
+                break;
             }
-            else{
-                if(str1.find(str2)!=string::npos){
-                    q.insert(str2);
-                }
-            }
+            
         }
-     }vector<string> ans;
-     for(auto x:q){
-         ans.push_back(x);
-     } return ans; 
+     }
+    return ans; 
     }
 };
