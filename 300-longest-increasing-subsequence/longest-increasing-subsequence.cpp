a9036d731698b780378086893074514e4e618284
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-03-18 19:51:19 +0530

    Time: 53 ms (66.61%) | Memory: 14.2 MB (71.78%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -15,8 +15,8 @@ public:
     //can take INT_MIN for take but make sure take=max(take,1+solve(i+1,i,arr,dp)) otherwise it will lead to take as INT_MIN in some case and if not take is 0 then dp[i][prev+1]=INT_MIN;and here it dp[i][prev+1]=0 if max(take,nottake )only if not change take=max(take,1+solve(i+1,i,arr,dp)) but LIS cannot be less than 1;
     int lengthOfLIS(vector<int>& arr) {
         int n=arr.size();
-        vector<vector<int>> dp(n,vector<int> (n,-1));
-        return solve(0,-1,arr,dp);
+        //vector<vector<int>> dp(n,vector<int> (n,-1));
+        //return solve(0,-1,arr,dp);
         // for(int i=n-1;i>=0;i--){
         //    for(int prev=i-1;prev>=-1;prev--){
         //      int take=0;
@@ -29,5 +29,16 @@ public:
         //    }
         // }
         // return dp[0][0];
+
+        vector<int> dp(n,1);
+        for(int i=1;i<n;i++){
+           for(int j=0;j<i;j++){
+              if(arr[i]>arr[j]){
+                  dp[i]=max(dp[i],1+dp[j]);
+              }
+              
+           }
+        }
+        return *max_element(dp.begin(),dp.end());
     }
 };
