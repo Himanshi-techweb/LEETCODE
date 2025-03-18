02a81ef4b32a5a348004b571fd8032dcb7a006ca
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-03-18 16:52:16 +0530

    Time: 612 ms (5.90%) | Memory: 295.3 MB (28.91%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -13,9 +13,21 @@ public:
         return dp[i][prev+1]=max(take,nottake);
     }//taking 0 ensure it always return non negative LIS and for base case it handle empty sequence
     //can take INT_MIN for take but make sure take=max(take,1+solve(i+1,i,arr,dp)) otherwise it will lead to take as INT_MIN in some case and if not take is 0 then dp[i][prev+1]=INT_MIN;and here it dp[i][prev+1]=0 if max(take,nottake )only if not change take=max(take,1+solve(i+1,i,arr,dp)) but LIS cannot be less than 1;
-    int lengthOfLIS(vector<int>& num) {
-        int n=num.size();
-        vector<vector<int>> dp(n,vector<int> (n+1,-1));
-        return solve(0,-1,num,dp);
+    int lengthOfLIS(vector<int>& arr) {
+        int n=arr.size();
+        vector<vector<int>> dp(n,vector<int> (n,-1));
+        return solve(0,-1,arr,dp);
+        // for(int i=n-1;i>=0;i--){
+        //    for(int prev=i-1;prev>=-1;prev--){
+        //      int take=0;
+        //      if( prev==-1 || arr[i]>arr[prev]){
+        //         take=1+dp[i+1][i+1];//ensure prev+1
+        //      }
+        // //not take
+        //      int nottake=dp[i+1][prev+1];
+        //      dp[i][prev+1]=max(take,nottake);
+        //    }
+        // }
+        // return dp[0][0];
     }
 };
