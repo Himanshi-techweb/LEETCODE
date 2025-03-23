20dbb95a01e7785fd79a480f8ce02d9f64c84d8c
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-03-23 14:33:10 +0530

    Time: 3 ms (91.44%) | Memory: 38.1 MB (36.80%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    int countPaths(int n, vector<vector<int>>& roads) {
-     vector<long long> way(n,0);
-     vector<long long> dist(n,LLONG_MAX);
-     priority_queue<pair<long long,long long>,vector<pair<long long,long long>>,greater<pair<long long,long long>>> q;//dist,node;
-     vector<vector<pair<long long,long long>>> arr(n);
-     for(auto it:roads){
-        arr[it[1]].push_back({it[0],it[2]});
-        arr[it[0]].push_back({it[1],it[2]});
-     }
-     q.push({0,0}) ;
-     dist[0]=0;
-     way[0]=1;
-     long long modulo=(1e9)+7;
-     while(!q.empty()){
+    int countPaths(int n, vector<vector<int>>& road) {
+       vector<long long> dist(n,LLONG_MAX);
+       vector<long long> way(n,0);
+       long long mod=(1e9)  + 7;
+       priority_queue<pair<long ,long>,vector<pair<long,long>>,greater<pair<long ,long>>> q;//{length,node}
+       vector<vector<pair<long,long>>>arr(n);
+       for(int i=0;i<road.size();i++){
+        arr[road[i][0]].push_back({road[i][1],road[i][2]});
+        arr[road[i][1]].push_back({road[i][0],road[i][2]});
+       }
+       dist[0]=0;
+       way[0]=1;
+       q.push({0,0});
+       while(!q.empty()){
         long long length=q.top().first;
         long long node=q.top().second;
         q.pop();
         for(auto it:arr[node]){
             if(length+it.second<dist[it.first]){
+                dist[it.first]=length+it.second;
                 way[it.first]=way[node];
-                dist[it.first]=(length+it.second);
                 q.push({length+it.second,it.first});
             }
             else if(length+it.second==dist[it.first]){
-                way[it.first]=(way[it.first]+way[node])%modulo;
+                way[it.first]=(way[node]+way[it.first])%mod;
             }
         }
-     }
-     return (way[n-1])%modulo;
+       }return way[n-1]%mod;
     }
 };
