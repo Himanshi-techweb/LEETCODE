2fe0badd94daaf3e4fb8e3678633902a4189a0e5
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-04-04 22:01:21 +0530

    Time: 6 ms (8.97%) | Memory: 23.5 MB (14.45%) - LeetSync

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+       queue<TreeNode*> q;
+       q.push(root);
+       map<TreeNode*,TreeNode*> check;
+       check[root]=root;
+       vector<TreeNode*> deepest;
+       while(!q.empty()){
+          int x=q.size();
+          deepest.clear();
+          for(int i=0;i<x;i++){
+            TreeNode* node=q.front();
+            q.pop();
+            deepest.push_back(node);
+            if(node->left){
+                q.push(node->left);
+                check[node->left]=node;
+            }
+            if(node->right){
+                q.push(node->right);
+                check[node->right]=node;
+            }
+          }
+       }
+       while(deepest.size()>1){
+          for(int i=0;i<deepest.size();i++){
+            deepest[i]=check[deepest[i]];
+          }
+          deepest.erase(unique(deepest.begin(),deepest.end()),deepest.end());
+       } 
+       return deepest[0];
+    }
+};
