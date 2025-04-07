4fc34139db65fed0fbfeb7503a69a4cc6dd0f56b
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-04-07 19:29:35 +0530

    Time: 195 ms (12.90%) | Memory: 101.1 MB (17.78%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
-    bool solve(int i,int tar,vector<int> &arr,vector<vector<int>> &dp){
-        if(tar==0)return true;
-        if(i==0)return arr[0]==tar;
-        if(dp[i][tar]!=-1)return dp[i][tar];
-        bool nottake=solve(i-1,tar,arr,dp);
-        bool take=(tar>=arr[i])?solve(i-1,tar-arr[i],arr,dp):false;
-        return dp[i][tar]=nottake || take;
-    }
-    bool canPartition(vector<int>& nums) {
-     int sum=0;
-     for(auto x:nums){
-        sum+=x;
-     } 
-     int n=nums.size();
-     if(sum%2!=0)return false;
-        int tar=sum/2;
-        vector<vector<bool>> dp(n,vector<bool> (tar+1,false));
-        //  return solve(n-1,tar,nums,dp);
-        for(int j=0;j<n;j++){
-          dp[j][0]=true;
-        }
-        if(nums[0]==tar)dp[0][nums[0]]=true;
+    bool solve(int i,int sum,int tar,vector<int> &arr,vector<vector<int>> &dp){
+        if(sum==tar)return true;
+        if(i>=arr.size()|| sum>tar)return false;
         
-        for(int i=1;i<n;i++){
-            for(int j=1;j<=tar;j++){
-                bool nottake=dp[i-1][j];
-                bool take=(j>=nums[i])?dp[i-1][j-nums[i]]:false;
-                 dp[i][j]=nottake || take; 
-            }
+        if(dp[i][sum]!=-1){
+            return dp[i][sum]==1?true:false;
         }
-        return dp[n-1][tar]; 
+        bool take=solve(i+1,sum+arr[i],tar,arr,dp);
+        bool nottake=solve(i+1,sum,tar,arr,dp);
+        dp[i][sum]=((take|| nottake)==true?1:0);
+        return dp[i][sum];
+    }
+    bool canPartition(vector<int>& arr) {
+      int sum=0;
+      for(auto x:arr){
+        sum+=x;
+      }
+      if(sum%2!=0)return false;
+      int tar=sum/2;
+      vector<vector<int>> dp(arr.size()+1,vector<int> (tar+1,-1));
+      return solve(0,0,tar,arr,dp);
     }
 };
