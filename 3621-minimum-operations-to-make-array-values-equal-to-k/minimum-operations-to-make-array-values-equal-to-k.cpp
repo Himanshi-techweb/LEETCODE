e669048a4cc6581d8d3c2e736e84794a945ef0a6
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-04-10 00:38:23 +0530

    Time: 4 ms (95.60%) | Memory: 32.4 MB (57.55%) - LeetSync

--- 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.cpp
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
-    int minOperations(vector<int>& nums, int k) {
-      for (int num : nums) {
-        if (num < k)
-            return -1;
-    }
-
-    // Use a set to store unique values greater than k
-    unordered_set<int> greaterThanK;
-    for (int num : nums) {
-        if (num > k)
-            greaterThanK.insert(num);
-    }
-
-    // The number of operations is equal to the number of unique elements > k
-    return greaterThanK.size();  
+    int minOperations(vector<int>& arr, int k) {
+     unordered_set<int> q;
+     for(auto x: arr){
+        if(x<k)return -1;
+        else if(x==k) continue;
+        else q.insert(x);
+     }
+     return q.size();
     }
 };
