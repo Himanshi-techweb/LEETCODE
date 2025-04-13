3905e6f850fce718fbc83af05e8f35623281fafd
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-04-13 18:17:37 +0530

    Time: 11 ms (44.95%) | Memory: 16 MB (6.97%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-// ans.push_back(check[temp]);
-            // ans.push_back(i);
-            //   for(int i=0;i<arr.size();i++){
-    //       check[arr[i]]=i;
-    //   }
-    vector<int> twoSum(vector<int>& arr, int target) {
-      unordered_map<int,int> check;
+    vector<int> twoSum(vector<int>& arr, int tar) {
+      unordered_map<int,int> check;vector<int> ans(2,-1);
       for(int i=0;i<arr.size();i++){
-        if(check.find(target-arr[i])!=check.end() && check[target-arr[i]]!=i){
-            
-            return {check[target-arr[i]],i};
-        }
-        check.insert({arr[i],i});
+        check[arr[i]]=i;
       }
-      return {};
+      for(int i=0;i<arr.size();i++){
+        int tar2=tar-arr[i];
+        if(check.find(tar2)!=check.end() && i!=check[tar2]){
+          ans[0]=i;
+          ans[1]=check[tar2];
+        }
+      } return ans; 
     }
 };
