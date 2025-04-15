b67454ab1e5f51d0978097948998911966021f36
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-04-15 19:35:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (60.56%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,16 +1,51 @@
 class Solution {
 public:
+    // vector<int> majorityElement(vector<int>& arr) {
+    //    unordered_map<int,int> check;int n=arr.size();
+    //    vector<int> ans;
+    //    for(auto x:arr){
+    //       check[x]++;
+    //    }
+    //    for(auto x:check){
+    //     if(x.second>n/3){
+    //         ans.push_back(x.first);
+    //     }
+    //    }
+    //    return ans; 
+    // }
+
+
     vector<int> majorityElement(vector<int>& arr) {
-       unordered_map<int,int> check;int n=arr.size();
-       vector<int> ans;
+       int count1=0;int count2=0;
+       int ele1=0;int ele2=0;
        for(auto x:arr){
-          check[x]++;
-       }
-       for(auto x:check){
-        if(x.second>n/3){
-            ans.push_back(x.first);
+        if(count1==0 &&  x!=ele2){
+            count1=1;
+            ele1=x;
+        }
+        else if(count2==0 &&  x!=ele1){
+            count2=1;
+            ele2=x;
+        }
+        else if(x==ele1){
+            count1++;
+        }
+        else if(x==ele2){
+            count2++;
+        }
+        else{
+            count1--;count2--;
         }
        }
-       return ans; 
+       count1=0;count2=0;
+       for(auto x: arr){
+        if(ele1==x)count1++;
+        if(ele2==x)count2++;
+       }
+       int check=(int)arr.size()/3  + 1;set<int> ans;
+       if(count1>=check)ans.insert(ele1);
+       if(count2>=check)ans.insert(ele2);
+       vector<int> result(ans.begin(),ans.end());
+       return result;
     }
 };
