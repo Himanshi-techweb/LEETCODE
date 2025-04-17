c23e4403182409d8abf162245a5981f387925883
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-04-18 00:53:21 +0530

    Time: 2 ms (33.67%) | Memory: 16.2 MB (6.09%) - LeetSync

--- 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.cpp
@@ -4,12 +4,9 @@ public:
         int n=arr.size();int count=0;
         unordered_map<int,vector<int>> check;
         for(int j=0;j<arr.size();j++){
-           if(check.find(arr[j])!=check.end()){
-             vector<int> curr=check[arr[j]];
-             for(auto i:curr){
-                if((i*j)%k==0)count++;
+             for(auto i:check[arr[j]]){
+                if(i<j && (i*j)%k==0)count++;
              }
-           }
            check[arr[j]].push_back(j);
         } 
          return count;
