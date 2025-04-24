df09489ddae9de8a3c0c92509830ff8f9890a682
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-04-24 19:16:33 +0530

    Time: 8 ms (96.52%) | Memory: 42 MB (49.93%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int countCompleteSubarrays(vector<int>& arr) {
-      set<int> check;
+      set<int> q;
       for(auto x:arr){
-       check.insert(x);
+       q.insert(x);
       }  
-      int k=check.size();int ans=0;
-      for(int i=0;i<arr.size();i++){
-        set<int> q;
-        for(int j=i;j<arr.size();j++){
-            q.insert(arr[j]);
-            if(q.size()==k)ans++;
-        }
+      unordered_map<int,int> check;
+      int k=q.size();int ans=0;int l=0;int r=0;
+      while( r<arr.size()){
+         check[arr[r]]++;
+         while(check.size()==k){
+            ans+=(arr.size()-r);
+            check[arr[l]]--;
+            if(check[arr[l]]==0)check.erase(arr[l]);
+            l++;
+         }
+         r++; 
       }
       return ans;
     }
