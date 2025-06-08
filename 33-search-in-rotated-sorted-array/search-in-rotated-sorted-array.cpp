4455bd299b925185334bd34dee877d1d9e68c661
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-08 17:20:59 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (66.63%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    void solve(int st,int end,int target ,vector<int> &arr,int &ans){
-        if(st>end)return ;
-        int pivot=(st+end)/2;
-        if(arr[pivot]==target){
-           ans=pivot; return;
-        } 
-        if(arr[st]<=arr[pivot] ){
-            if(arr[st]<=target && target<arr[pivot]){
-                solve(st,pivot-1,target,arr,ans);
-            }else{
-                solve(pivot+1,end,target,arr,ans);
-            }
+    int search(vector<int>& arr, int tar) {
+      int low=0;
+      int high=arr.size()-1;  
+      int ans=-1;
+      while(low<=high){
+        int mid=(low+high)/2;
+        if(arr[mid]==tar)ans=mid;
+        if(arr[low]<=arr[mid]){//sorted left
+           if(tar >= arr[low] && tar<arr[mid]){
+              high=mid-1;
+           }
+           else{
+            low=mid+1;
+           }
         }
-        else{
-            if(arr[pivot]<target && target <=arr[end]){
-                solve(pivot+1,end,target,arr,ans);
-            }
-            else{
-                solve(st,pivot-1,target,arr,ans);
-            }
+        
+        else {//sorted right
+           if(tar>arr[mid] && tar<=arr[high]){
+              low=mid+1;
+           }
+           else{
+            high=mid-1;
+           }
         }
-    }
-    int search(vector<int>& nums, int target) {
-        if(nums.size()==1 && nums[0]==target)return 0;
-       int st=0;
-       int end=nums.size()-1;
-       int ans=-1;
-       solve(st,end,target,nums,ans);
-       return ans;
+        
+      }return ans; 
     }
 };
