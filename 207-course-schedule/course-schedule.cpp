fb7e44d31dd8ef2349c9b5f4ad7458871c8bf5a3
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-17 13:23:44 +0530

    Time: 1 ms (86.96%) | Memory: 19.3 MB (75.37%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    bool canFinish(int x, vector<vector<int>>& arr) {
-       if(arr.size()==0)return true;
-       queue<int> q;
-       vector<vector<int>> check(x);
-       for(int i=0;i<arr.size();i++){
-            check[arr[i][1]].push_back(arr[i][0]);
+    // bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+    // }
+    bool canFinish(int V, vector<vector<int>> &edge) {
+       vector<vector<int>>arr(V);
+       vector<int> in(V,0);
+       queue<int>q;
+       for(int i=0;i<edge.size();i++){
+        arr[edge[i][0]].push_back(edge[i][1]);
+        in[edge[i][1]]++;
        }
-       vector<int> in(x,0);
-       for(int i=0;i<x;i++){
-        for(auto it:check[i]){
-            in[it]++;
-        }
-       } 
-       for(int i=0;i<x;i++){
-        if(in[i]==0)q.push(i);
+       for(int i=0;i<V;i++){
+           if(in[i]==0)q.push(i);
        }
-       vector<int> ans;
        while(!q.empty()){
-         int j=q.front();
-         ans.push_back(j);
-         q.pop();
-         for(auto it:check[j]){
-            in[it]--;
-            if(in[it]==0)q.push(it);
-         }
-         
+           int front=q.front();q.pop();
+           for(auto it:arr[front]){
+               in[it]--;
+               if(in[it]==0)q.push(it);
+           }
        }
-       return x==ans.size();
+       for(auto it:in){
+           if(it>0)return false;
+       }
+       return true;
     }
 };
