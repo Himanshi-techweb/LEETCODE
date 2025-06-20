837668207a1facbb3b415d714b9b91efbcb7dd67
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-20 11:18:17 +0530

    Time: 180 ms (5.03%) | Memory: 78.9 MB (5.00%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-       int m=mat.size();
-       int n=mat[0].size();
-       vector<vector<int>> ans(m,vector<int> (n,-1));
-       queue<pair<int,int>> q;
-       for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
+       queue<pair<pair<int,int>,int>> q;
+       vector<vector<int>> visit(mat.size(),vector<int> (mat[0].size(),0));
+       vector<vector<int>> ans=mat;
+       vector<vector<int>> rc={{0,-1},{-1,0},{0,1},{1,0}};
+       for(int i=0;i<mat.size();i++){
+        for(int j=0;j<mat[0].size();j++){
             if(mat[i][j]==0){
-                q.push({i,j});
-                ans[i][j]=0;
+                q.push({{i,j},0});
+                visit[i][j]=1;
             }
         }
        }
-    //    vector<pair<int,int>> check;
-    vector<int> rowcheck={0,-1,0,1};
-    vector<int> colcheck={-1,0,1,0};
-    //    check={{0,-1},{-1,0},{0,1},{1,0}};
        while(!q.empty()){
-          int row=q.front().first;
-          int col=q.front().second;
+          int row=q.front().first.first;
+          int col=q.front().first.second;
+          int wt=q.front().second;
           q.pop();
-          for(int i=0;i<4;i++){
-             int nrow=row+rowcheck[i];
-             int ncol=col+colcheck[i];
-             if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && ans[nrow][ncol]==-1){
-                ans[nrow][ncol]=1+ans[row][col];
-                q.push({nrow,ncol});
-             }
+          for(auto it:rc){
+            int nr=row+it[0];
+            int nc=col+it[1];
+            if(nr>=0 && nr<mat.size()  && nc>=0 && nc<mat[0].size()  && visit[nr][nc]==0 && mat[nr][nc]==1){
+                q.push({{nr,nc},wt+1});
+                visit[nr][nc]=1;
+                ans[nr][nc]=wt+1;
+            }
           }
-       } 
+       }
        return ans;
     }
 };
