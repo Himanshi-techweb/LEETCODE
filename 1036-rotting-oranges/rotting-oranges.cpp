6b445e0cca20c9170b4b45eefdeaa8f45e8f1744
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-20 10:44:36 +0530

    Time: 3 ms (30.67%) | Memory: 18.4 MB (5.32%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int count =0;
-        int fresh=0;
-        vector<pair<int,int>> check;
-        check={{0,-1},{0,1},{-1,0},{1,0}};
-        queue<pair<int,int>> q;
-        
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-               if(grid[i][j]==2){
-                 q.push({i,j});
-               }
-               if(grid[i][j]==1){
-                fresh++;
-               }
+       int m=grid.size();
+       int n=grid[0].size();
+       vector<vector<int>> visit(m,vector<int> (n,0));
+       int fresh=0;
+       queue<pair<int,int>>q;
+       vector<vector<int>> rc={{-1,0},{0,-1},{1,0},{0,1}};
+       for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+          if(grid[i][j]==2){
+            q.push({i,j});
+            visit[i][j]=1;
+          } 
+          else if(grid[i][j]==1)fresh++; 
+        }
+       }int cnt=0;
+       while(!q.empty() && fresh>0){
+        int size=q.size();
+        for(int i=0;i<size;i++){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            for(auto it:rc){
+            int nr=row+it[0];
+            int nc=col+it[1];
+            if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==1){
+                q.push({nr,nc});
+                visit[nr][nc]=1;
+                grid[nr][nc]=2;
+                fresh--;
             }
         }
-        if(fresh==0)return 0;
-        while(!q.empty()){
-            int size=q.size();
-            bool rotten=false;
-            for(int i=0;i<size;i++){
-                int frow=q.front().first;
-                int fcol=q.front().second;
-                q.pop();
-                for(auto x: check){
-                   int nrow=x.first+frow;
-                   int ncol=x.second+fcol;
-                   if(nrow>=0 && nrow<grid.size() && ncol>=0 && ncol<grid[0].size() && grid[nrow][ncol]==1){
-                        q.push({nrow,ncol});
-                        grid[nrow][ncol]=2;
-                        rotten=true;
-                        fresh--;
-                   }
-                }
-            }if(rotten)count++;
         }
-        if(fresh!=0)return -1;
-        return count;
+        cnt++;
+       }
+       if(fresh>0)return -1;
+       return cnt; 
     }
 };
