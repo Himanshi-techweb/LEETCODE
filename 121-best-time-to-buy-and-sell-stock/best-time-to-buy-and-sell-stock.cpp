0b65293a968141e394fad0d81ce67f8333364d90
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-23 23:06:51 +0530

    Time: 7 ms (7.02%) | Memory: 105.4 MB (5.75%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int q=prices.size();
-        // vector<int> before(q,0);
-        // vector<int> after(q,0);
-        // after[q-1]=prices[q-1];
-        // before[0]=prices[0];
-        // for(int i=1;i<q;i++){
-        //     before[i]=min(prices[i],before[i-1]);
-        // }
-        // for(int i=q-2;i>=0;i--){
-        //     after[i]=max(prices[i],after[i+1]);
-        // }
-        int profit=0;
-        int mini=prices[0];
-        for(int i=1;i<q;i++){
-           profit=max(profit,prices[i]-mini);
-           mini=min(mini,prices[i]);
-        }return profit;
+    int maxProfit(vector<int>& arr) {
+        int m=arr.size();
+        vector<int> left(m,0);
+        left[m-1]=arr[m-1];
+        vector<int> right(m,0);
+        right[0]=arr[0];
+        for(int i=m-2;i>=0;i--){
+           left[i]=max(left[i+1],arr[i]);
+        }
+        for(int i=1;i<m;i++){
+            right[i]=min(right[i-1],arr[i]);
+        }
+        int maxi=0;
+        for(auto int i=0;i<m;i++){
+            maxi=max(maxi,left[i]-right[i]);
+        }
+        return maxi;
     }
 };
