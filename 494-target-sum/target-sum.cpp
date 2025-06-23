dc71998a381d89151dbc31e4fbda91691b4f799f
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-23 11:48:20 +0530

    Time: 211 ms (32.81%) | Memory: 11.6 MB (83.37%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    int solve(int i,int tar,vector<int>&arr,vector<vector<int>> &dp){
+    int solve(int i,int tar,vector<int> &arr,int total){
         if(i==0){
-            if(arr[0]==0 && tar==0)return 2;
-            else if(tar==0 || arr[0]==tar)return 1;
-            else return 0;
+            if(tar==arr[0] && tar==0)return 2;
+            else if(tar==arr[0] || tar==0)return 1;
+            return 0;
         }
-        if(dp[i][tar]!=-1)return dp[i][tar];
-        int nottake=solve(i-1,tar,arr,dp);
         int take=0;
-        if(arr[i]<=tar)take=solve(i-1,tar-arr[i],arr,dp);
-        return dp[i][tar]=(take+nottake);
+        if(arr[i]<=tar)take=solve(i-1,tar-arr[i],arr,total);
+        int nottake=solve(i-1,tar,arr,total);
+        return take+nottake;
     }
-    int findTargetSumWays(vector<int>& nums, int target) {
-       int sum=accumulate(nums.begin(),nums.end(),0);
-       int n=nums.size();
-       int tar=target+sum;
-       if(tar%2!=0 || tar<0)return 0;
-       tar=tar/2;
-       vector<vector<int>> dp(n,vector<int> (tar+1,-1));
-       return solve(n-1,tar,nums,dp);
-       
+    int findTargetSumWays(vector<int>& arr, int target) {
+       int total=accumulate(arr.begin(),arr.end(),0);
+       int m=arr.size();
+       if((total+target)%2!=0)return 0;
+       target=(total+target)/2;
+       return solve(m-1,target,arr,total);
     }
 };
