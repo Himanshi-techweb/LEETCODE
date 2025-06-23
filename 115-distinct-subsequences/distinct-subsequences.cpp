eb96fabd77265cd4d00db13f841d2405d1af140d
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-23 19:04:27 +0530

    Time: 14 ms (85.56%) | Memory: 8.7 MB (99.04%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,38 +1,25 @@
 class Solution {
 public:
-    int solve(int i,int j,string t1,string t2, vector<vector<int>> &dp){
-        if(j<=0)return 1;
-        if(i<=0)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(t1[i-1]==t2[j-1]){
-           return  dp[i][j]=solve(i-1,j-1,t1,t2,dp)+solve(i-1,j,t1,t2,dp);
-        }
-        else{
-            return dp[i][j]=solve(i-1,j,t1,t2,dp);
-        }
-    }
-    int numDistinct(string t1, string t2) {
-       int q1=t1.size();
-       int q2=t2.size();
-       vector<vector<unsigned long long>> dp(q1+1,vector<unsigned long long> (q2+1,0ULL));
-       for(int i=0;i<=q1;i++){
-         dp[i][0]=1;
-       }
-    //    return solve(q1,q2,t1,t2,dp); 
-    vector<double> prev(q2+1,0),curr(q2+1,0);
-    prev[0]=curr[0]=1;
-
-    for(int i=1;i<=q1;i++){
-        for(int j=q2;j>=1;j--){
-            if(t1[i-1]==t2[j-1]){
-               prev[j]=prev[j-1]+prev[j];
+    int numDistinct(string x, string y) {
+       int m=x.size();int n=y.size();
+    //    vector<vector<long long>> dp(m+1,vector<long long> (n+1,0));
+       vector<unsigned int> prev(n+1,0);
+       vector<unsigned int> curr(n+1,0);
+       prev[0]=1;curr[0]=1;
+    //    for(int i=0;i<=m;i++){
+    //     dp[i][0]=1;
+    //    }
+       for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+            if(x[i-1]==y[j-1]){
+                curr[j]=prev[j-1]+prev[j];
             }
             else{
-                 prev[j]=prev[j];
+                curr[j]=prev[j];
             }
         }
-        //prev=curr;
-    }
-    return (int)prev[q2];//typecast return double to int
+        prev=curr;
+       }
+       return prev[n];
     }
 };
