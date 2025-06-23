9585aa81b94ba8e827feef131b01ab7a6d7f6cad
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-23 15:48:32 +0530

    Time: 26 ms (63.55%) | Memory: 27.5 MB (50.98%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
-    int solve(int i1,int i2,string &text1, string &text2,vector<vector<int>>&dp){
-        if(i1<0 || i2<0)return 0;
-       if(dp[i1][i2]!=-1)return dp[i1][i2];
-       //match
-    //    int match=-1e9;
-    //    if(text1[i1]==text2[i2]){
-    //     match=1+solve(i1-1,i2-1,text1,text2,dp);
-    //    }
-    //    //not match
-    //    int notmatch=max(solve(i1,i2-1,text1,text2,dp),solve(i1-1,i2,text1,text2,dp));
-    //    return dp[i1][i2]=max(match,notmatch);
-       if(text1[i1]==text2[i2]){
-        return dp[i1][i2]=1+solve(i1-1,i2-1,text1,text2,dp);
-       }
-       //not match
-       return dp[i1][i2]=max(solve(i1,i2-1,text1,text2,dp),solve(i1-1,i2,text1,text2,dp));
+    int solve(int i,int j,string x,string y,vector<vector<int>> &dp){
+        if(i<0 || j<0)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(x[i]==y[j])return 1+solve(i-1,j-1,x,y,dp);
+        int left=INT_MIN;int right=INT_MIN;
+        left=solve(i-1,j,x,y,dp);
+        right=solve(i,j-1,x,y,dp);
+        return dp[i][j]= max(left,right);
     }
-    int longestCommonSubsequence(string text1, string text2) {
-       int n1=text1.size();
-       int n2=text2.size();
-       vector<vector<int> >dp(n1+1,vector<int> (n2+1,0));
-       //return solve(n1-1,n2-1,text1,text2,dp);
-      for(int i=0;i<=n2;i++){
-        dp[0][i]=0;
-      }
-      for(int i=0;i<=n1;i++){
-        dp[i][0]=0;
-      } 
-      for(int i=1;i<=n1;i++){
-        for(int j=1;j<=n2;j++){
-           if(text1[i-1]==text2[j-1]){
-           dp[i][j]=1+dp[i-1][j-1];
-           }
-           else dp[i][j]=max(dp[i][j-1],dp[i-1][j]); 
+    int longestCommonSubsequence(string x, string y) {
+       int m=x.size();
+       int n=y.size();
+       vector<vector<int>> dp(m+1,vector<int> (n+1,0));
+       vector<int> prev(n+1,0);
+    //    return solve(i-1,j-1,text1,text2,dp);
+        for(int i=0;i<=m;i++){
+            dp[i][0]=0;
+        } 
+        for(int j=0;j<=n;j++){
+            dp[0][j]=0;
+        } 
+        for(int i=1;i<=m;i++){
+          for(int j=1;j<=n;j++){
+            if(x[i-1]==y[j-1])dp[i][j]= 1+dp[i-1][j-1];
+            else{
+              int left=INT_MIN;int right=INT_MIN;
+              left=dp[i-1][j];
+              right=dp[i][j-1];
+              dp[i][j]= max(left,right);
+            }
+          }
         }
-      } 
-      return dp[n1][n2];
+        return dp[m][n];
     }
 };
