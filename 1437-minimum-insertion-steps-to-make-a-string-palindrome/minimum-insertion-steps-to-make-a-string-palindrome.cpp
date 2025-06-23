a9d00df1c1806afdbb6eb340f10d334d8b3c3179
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-23 18:00:03 +0530

    Time: 35 ms (49.15%) | Memory: 31.6 MB (37.37%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    int minInsertions(string s) {
-      string t1=s;
-      reverse(s.begin(),s.end());
-      string t2=s;
-      int k=s.size();
-    //   vector<vector<int>>dp(k+1,vector<int> (k+1,0));
-    vector<int> prev(k+1,0);
-    vector<int> curr(k+1,0);
-    //   for(int i=0;i<=k;i++){
-    //     dp[0][i]=0;
-    //     dp[i][0]=0;
-    //   }
-    prev[0]=curr[0]=0;
-      for(int i=1;i<=k;i++){
-        for(int j=1;j<=k;j++){
-            if(t1[i-1]==t2[j-1]){
-                curr[j]=1+prev[j-1];
-            }
+    int minInsertions(string x) {
+       string y=x;
+       reverse(y.begin(),y.end());
+       int m=x.size();
+       int n=y.size();
+       vector<vector<int>> dp(m+1,vector<int> (n+1,0));
+       vector<int> prev(n+1,0);
+        for(int i=0;i<=m;i++){
+            dp[i][0]=0;
+        } 
+        for(int j=0;j<=n;j++){
+            dp[0][j]=0;
+        } 
+        for(int i=1;i<=m;i++){
+          for(int j=1;j<=n;j++){
+            if(x[i-1]==y[j-1])dp[i][j]= 1+dp[i-1][j-1];
             else{
-                curr[j]=max(curr[j-1],prev[j]);
+              int left=dp[i-1][j];
+              int right=dp[i][j-1];
+              dp[i][j]= max(left,right);
             }
+          }
         }
-        prev=curr;
-      } 
-      return k-prev[k];
+        return m-dp[m][n];
     }
 
 };
