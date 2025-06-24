ca7bb14a873646fc249e0c35a0ce82603fc59b93
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-24 17:27:47 +0530

    Time: 15 ms (31.19%) | Memory: 12.4 MB (93.96%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
+    int solve(int i,int prev,vector<int> &arr){
+        if(i==arr.size())return 0;
+        int len=solve(i+1,0,arr);
+        if(prev==-1 || (i==prev+1 && ( arr[i]%arr[prev]==0 || arr[prev]%arr[i]==0))){
+            len=1+solve(i+1,i,arr);
+        }
+        return len;
+    }
     vector<int> largestDivisibleSubset(vector<int>& arr) {
         int n=arr.size();
-       vector<int> dp(n,1); 
-       vector<int> check(n);
-       for(int i=0;i<n;i++){
-        check[i]=i;
-       }
-       int ix=0;int ans=0;
-       sort(arr.begin(),arr.end());
-       for(int i=1;i<n;i++){
-        for(int j=0;j<i;j++){
-            if(arr[i]%arr[j]==0 && dp[j]+1>dp[i]){
-                dp[i]=dp[j]+1;
-                check[i]=j;
+        vector<int> dp(n,1);
+        vector<int> back(n);
+        vector<int> ans;
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<n;i++)back[i]=i;
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+                if(((arr[i]%arr[prev]==0) || (arr[prev]%arr[i]==0)) && dp[i]<1+dp[prev]){
+                    back[i]=prev;
+                    dp[i]=1+dp[prev];
+                }
+            }
+        }int maxi=dp[0];int ix=0;
+        for(int i=0;i<n;i++){
+            if(dp[i]>maxi){
+                maxi=dp[i];
+                ix=i;
             }
         }
-        if(ans<dp[i]){
-            ans=dp[i];
-            ix=i;
+        ans.push_back(arr[ix]);
+        while(ix!=back[ix]){
+          ix=back[ix];
+          ans.push_back(arr[ix]);
         }
-       }
-       vector<int> result;
-       while(ix!=check[ix]){
-         result.push_back(arr[ix]);
-         ix=check[ix];
-       }
-       result.push_back(arr[ix]);
-       //reverse(result.begin(),result.end());
-       return result;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
