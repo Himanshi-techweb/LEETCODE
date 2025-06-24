3cfe733d1ed797c5d7b7b0309ad95dead31ac348
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-24 16:17:11 +0530

    Time: 60 ms (63.14%) | Memory: 14.1 MB (79.24%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -11,17 +11,30 @@ public:
     //     return dp[i][prev+1]=len;
     // }
     int lengthOfLIS(vector<int>& arr) {
-        vector<vector<int>> dp(arr.size()+1,vector<int> (arr.size()+1,0));
-        // return solve(0,-1,arr,dp);
-        for(int i=arr.size()-1;i>=0;i--){
-            for(int prev=arr.size()-2;prev>=-1;prev--){
-                int len=dp[i+1][prev+1];
-                if(prev==-1 || arr[i]>arr[prev]){
-                   len=max(len,1+dp[i+1][i+1]);
+        // vector<vector<int>> dp(arr.size()+1,vector<int> (arr.size()+1,0));
+        // // return solve(0,-1,arr,dp);
+        // for(int i=arr.size()-1;i>=0;i--){
+        //     for(int prev=i-1;prev>=-1;prev--){
+        //         int len=dp[i+1][prev+1];
+        //         if(prev==-1 || arr[i]>arr[prev]){
+        //            len=max(len,1+dp[i+1][i+1]);
+        //         }
+        //         dp[i][prev+1]=len; 
+        //     }
+        // }
+        // return dp[0][0];
+        int n=arr.size();
+        vector<int> dp(n,1);
+        vector<int> back;
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+                if(arr[i]>arr[prev]){
+                    dp[i]=max(dp[i],1+dp[prev]);
                 }
-                dp[i][prev+1]=len; 
             }
+            maxi=max(dp[i],maxi);
         }
-        return dp[0][0];
+        return maxi;
     }
 };
