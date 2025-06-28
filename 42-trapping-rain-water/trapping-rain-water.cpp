cf7de7a8b95962f2ed141c5f11a90bc1486c700b
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-28 16:09:29 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (39.72%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    int trap(vector<int>& arr) {
-     int n=arr.size();
-     int leftmax=0;
-     int rightmax=0;
-     int total=0;
-     int l=0;int r=n-1;
-     while(l<r){
-        if(arr[l]<=arr[r]){
-           if(leftmax>arr[l]){
-               total+=leftmax-arr[l]; 
-           } 
-           else{
-               leftmax=arr[l];
-           }
-           l++;
+    int trap(vector<int>& height) {
+        int n=height.size();
+      vector<int> prefix(n);
+      prefix[0]=height[0];
+      vector<int> suffix(n);
+      suffix[n-1]=height[n-1];
+      int ans=0;
+      for(int i=1;i<n;i++){
+        prefix[i]=max(prefix[i-1],height[i]);
+      } 
+      for(int i=n-2;i>=0;i--){
+        suffix[i]=max(suffix[i+1],height[i]);
+      }
+      for(int i=0;i<n;i++){
+        if(prefix[i]>=height[i] && suffix[i]>=height[i]){
+            ans+=(min(suffix[i],prefix[i])-height[i]);
         }
-        else{
-            if(rightmax>=arr[r]){
-             total+=rightmax-arr[r];
-            }
-            else{
-              rightmax=arr[r];
-            } 
-            r--;
-        }
-     }
-     return total;
+      }
+      return ans; 
     }
 };
