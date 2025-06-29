a1e00e7bd6bfafd2404dd7da5db715b37100d3d7
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-29 19:30:08 +0530

    Time: 15 ms (74.74%) | Memory: 28.4 MB (53.65%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    int solve(int i,int tar,vector<int> &arr,vector<vector<int>> &dp){
+    long long solve(int i,int amount,vector<int> &arr,vector<vector<int>> &dp){
         if(i==0){
-            if(tar%arr[0]==0)return 1;
-            else return 0;
+            if(amount%arr[0]==0) return 1;
+            return 0;
         }
-        if(dp[i][tar]!=-1)return dp[i][tar];
-        int take=0;
-        if(arr[i]<=tar)take=solve(i,tar-arr[i],arr,dp);
-        int nottake=solve(i-1,tar,arr,dp);
-        return dp[i][tar]=take+nottake;
-    }  
-    int change( int amount,vector<int>& arr) {
-        int m=arr.size();
-        vector<vector<int>> dp(m+1,vector<int> (amount+1,-1));
-       return solve(m-1,amount,arr,dp); 
+        if(dp[i][amount]!=-1)return dp[i][amount];
+        long long take=0;
+        if(arr[i]<=amount)take=solve(i,amount-arr[i],arr,dp);
+        long long nottake=solve(i-1,amount,arr,dp);
+        return dp[i][amount]=take+nottake; 
+    }
+    int change(int amount,vector<int>& coins) {
+        vector<vector<int>> dp(coins.size(),vector<int> (amount+1,-1));
+        return solve(coins.size()-1,amount,coins,dp);
     }
 };
