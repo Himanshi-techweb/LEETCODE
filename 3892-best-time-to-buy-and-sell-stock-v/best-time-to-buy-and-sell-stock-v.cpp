566e0fdfcd8e086c1c2c12683475632898f76005
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-30 00:10:35 +0530

    Time: 244 ms (67.04%) | Memory: 142.9 MB (49.57%) - LeetSync

--- 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.cpp
+++ 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    long long solve(int i,int holding,int k,vector<int> &arr,vector<vector<vector<long long>>> &dp){
+    long long solve(int i,int cnt,int k,vector<int> &arr,vector<vector<vector<long long>>> &dp){
+        if(i>=arr.size())return 0;
         if(k==0)return 0;
-        if(i==arr.size())return 0;
+        if(dp[i][cnt][k]!=-1)return dp[i][cnt][k];
         if(i==arr.size()-1){
-            if(holding==0)return 0;
-            if(holding==1)return arr[i];
-            return -arr[i];
-        };
-        if(dp[i][holding][k]!=-1)return dp[i][holding][k];
+            if(cnt==0)return 0;
+            if(cnt==1)return arr[i];
+            if(cnt==2)return -arr[i];
+        }
         long long profit=INT_MIN;
-        profit=max(profit,solve(i+1,holding,k,arr,dp));
-        if(holding==0){
+        if(cnt==0){
             profit=max(profit,-arr[i]+solve(i+1,1,k,arr,dp));
+            profit=max(profit,solve(i+1,0,k,arr,dp));
             profit=max(profit,arr[i]+solve(i+1,2,k,arr,dp));
         }
-        else if(holding==1){
+        else if(cnt==1){
             profit=max(profit,arr[i]+solve(i+1,0,k-1,arr,dp));
+            profit=max(profit,solve(i+1,1,k,arr,dp));
         }
         else{
             profit=max(profit,-arr[i]+solve(i+1,0,k-1,arr,dp));
+            profit=max(profit,solve(i+1,2,k,arr,dp));
         }
-        return dp[i][holding][k]=profit;
+        return dp[i][cnt][k]=profit;
     }
-    long long maximumProfit(vector<int>& price,int k) {
-        vector<vector<vector<long long>>> dp(price.size(),vector<vector<long long>>(4,vector<long long> (k+1,-1)));
-       return solve(0,0,k,price,dp);
+    long long maximumProfit(vector<int>& prices, int k) {
+        // vector<vector<int
+        vector<vector<vector<long long>>> dp(prices.size()+1,vector<vector<long long>> (3,vector<long long> (k+1,-1)));
+        return solve(0,0,k,prices,dp);
     }
 };
