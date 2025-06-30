8f17437fb3af9eb3e36a02b87ecc1d25f02b1e75
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-30 18:18:27 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (45.79%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -2,39 +2,22 @@ class Solution {
 public:
     // void insertinterval()
     vector<vector<int>> insert(vector<vector<int>>& arr, vector<int>& ni) {
-        //insertinterval
-        if(arr.size()==0)return {ni};
         vector<vector<int>> ans;
-        int i=0;bool there=false;
-        while(i<arr.size()){
-           if(arr[i][0]>ni[0]){
-            ans.push_back(ni);
-            there=true;
-            break;
-           }
-           else{
-            ans.push_back(arr[i]);
-           }
-           i++;
-        }if(!there)ans.push_back(ni);
-        while(i<arr.size()){
+        int i=0;
+        while(i<arr.size() && arr[i][1]<ni[0]){
             ans.push_back(arr[i]);
             i++;
         }
-        
-        //merge
-        vector<vector<int>> result;
-        vector<int> curr=ans[0];
-        for(int i=1;i<ans.size();i++){
-           if(curr[1]>=ans[i][0]){
-            curr[1]=max(ans[i][1],curr[1]);
-           } 
-           else{
-            result.push_back(curr);
-            curr=ans[i];
-           }
+        while(i<arr.size() && arr[i][0]<=ni[1]){
+            ni[0]=min(arr[i][0],ni[0]);
+            ni[1]=max(arr[i][1],ni[1]);
+            i++;
         }
-        result.push_back(curr);
-        return result;
+        ans.push_back(ni);
+        while(i<arr.size() ){
+            ans.push_back(arr[i]);
+            i++;
+        }
+        return ans;
     }
 };
