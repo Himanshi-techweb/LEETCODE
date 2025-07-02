da41c4da9f538a82b0df6825897947b9e586e641
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-02 15:00:22 +0530

    Time: 91 ms (33.23%) | Memory: 88.9 MB (29.11%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -5,12 +5,11 @@ public:
        for(auto it:arr){
          st.insert(it);
        } 
-       for(int i=0;i<arr.size();i++){
-         if(st.find(arr[i]-1)==st.end()){
-            cnt=0;int check=arr[i];
-            while(st.find(check)!=st.end()){
+       for(auto it:st){
+         if(!st.count(it-1)){
+            cnt=1;int check=it;
+            while(st.count(check+1)){
                cnt++;
-               st.erase(check);
                check=check+1;
             }
             maxcnt=max(maxcnt,cnt);
