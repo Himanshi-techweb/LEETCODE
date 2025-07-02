b9187ebf894115bf22d2db57c07eacd1594b5acf
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-02 18:00:52 +0530

    Time: 47 ms (71.45%) | Memory: 29 MB (73.56%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& arr) {
-    //    vector<vector<int>> ans;
-    set<vector<int>> st;
+       vector<vector<int>> ans;
        sort(arr.begin(),arr.end());
        int l=0;int r=arr.size()-1;
        for(int i=0;i<arr.size();i++){
+          if(i-1>=0 && arr[i]==arr[i-1])continue;
           l=i+1;int sum=arr[i]; r=arr.size()-1;
           while(l<r){
             sum=(arr[l]+arr[r]+arr[i]);
             if(sum==0){
-                st.insert({arr[i],arr[l],arr[r]});
-                int a=arr[l];int b=arr[r];
-                while(l<=r && arr[l]==a)l++;
-                while(l<=r && arr[r]==b)r--;
+                ans.push_back({arr[i],arr[l],arr[r]});
+                int a=arr[l];int b=arr[r];l++;r--;
+                while(l<r && arr[l]==a)l++;
+                while(l<r && arr[r]==b)r--;
             }
             else if(sum<0)l++;
             else r--;
           }
-       }
-       vector<vector<int>>ans1; 
-       for(auto it:st){
-        ans1.push_back(it);
-       }
-       return ans1;
+       } 
+       return ans;
     }
 };
