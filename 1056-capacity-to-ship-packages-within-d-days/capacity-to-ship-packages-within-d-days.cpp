44cbeefcb2b4933d80efb96b9e0c6dbe51f086f0
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-04 19:57:15 +0530

    Time: 7 ms (92.01%) | Memory: 35.1 MB (8.69%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    bool check(int mid,int days,vector<int> &arr){
-        int i=0;int j=0;int day=1;
-        for(auto x: arr){
-          if(i+x>mid){
-            day++;
-            i=x;
-          }
-          else{
-            i+=x;
-          }
-        }
-        return day<=days;
+    bool check(int mid,vector<int> &arr,int days){
+       int cnt=0;int sum=0;
+       for(auto it:arr){
+         if(it>mid)return false;
+         if(sum+it<=mid){
+            sum=sum+it;
+         }
+         else{
+            cnt++;
+            sum=it;
+         }
+       }
+       cnt++;
+       return cnt<=days;
     }
     int shipWithinDays(vector<int>& arr, int days) {
-        int low=*max_element(arr.begin(),arr.end());int ans=0;
-        int high=accumulate(arr.begin(),arr.end(),0);
-        while(low<=high){
-            int mid=low + (high-low)/2;
-            if(check(mid,days,arr)){
-                ans=mid;
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
+      int low=*min(arr.begin(),arr.end());
+      int high=accumulate(arr.begin(),arr.end(),0);
+      int capacity=0;
+      while(low<=high){
+        int mid=low+(high-low)/2;
+        if(check(mid,arr,days)){
+            capacity=mid;
+            high=mid-1;
         }
-        return ans;
+        else{
+            low=mid+1;
+        }
+      } 
+      return capacity; 
     }
 };
