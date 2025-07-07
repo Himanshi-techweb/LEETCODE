6f35a77cfe76cb2590ad9983abf249092677cbfe
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-07 17:15:32 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (32.87%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int search(vector<int>& arr, int tar) {
-      int low=0;int high=arr.size()-1; 
-      while(low<=high){
+       int low=0;int high=arr.size()-1;
+       while(low<=high){
         int mid=low+(high-low)/2;
         if(arr[mid]==tar)return mid;
-        if(arr[mid]<arr[high]){
-            if(tar<=arr[high] && tar>arr[mid])low=mid+1;
-            else{
-                high=mid-1;
-            }
+        if(arr[low]<=arr[mid]){//left region is sort
+           if(arr[low]<=tar && tar<arr[mid])high=mid-1;
+           else low=mid+1;
         }
-        else{
-            if(arr[low]<=tar && tar<arr[mid])high=mid-1;
-            else low=mid+1;
+        else {
+          if(arr[mid]<tar && tar<=arr[high])low=mid+1;
+          else high=mid-1;
         }
-      } 
-      return -1;
+       } 
+       return -1;
     }
 };
