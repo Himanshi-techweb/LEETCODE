241271d3f0f19abd1c126a152a279d734faa7dfe
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-09 16:38:09 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (33.00%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& arr) {
-        int n=arr.size();
-       //finding corner point in array
-       int x=0;
-       for(int i=n-2;i>=0;i--){
-        if(arr[i]<arr[i+1]){
-           x=i;
-           break;
-        }
-       } 
-       //just next greater element
-       int mini=INT_MAX;int mini_ix=n-1;
-       for(int i=x+1;i<n;i++){
-         if(arr[i]>arr[x] && arr[i]<mini){
-            mini=arr[i];
-            mini_ix=i;
+        int ix=-1;
+       for(int i=arr.size()-2;i>=0;i--){
+         if(arr[i]<arr[i+1]){
+            ix=i;break;
          }
        }
-       swap(arr[x],arr[mini_ix]);
-       //sort just after x 
-       sort(arr.begin()+x+1,arr.end());
-       
+       if(ix==-1){
+        reverse(arr.begin(),arr.end());
+        
+       }
+       else{
+        for(int j=arr.size()-1;j>ix;j--){
+            if(arr[j]>arr[ix]){
+                swap(arr[ix],arr[j]);
+                break;
+            }
+            
+        }
+        reverse(arr.begin()+ix+1,arr.end());
+       }
     }
 };
