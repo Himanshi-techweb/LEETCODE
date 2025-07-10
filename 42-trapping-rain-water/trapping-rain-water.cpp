3ab861b7aafe281c9ec5f47dddca18da62c3c926
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-10 13:35:25 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (20.00%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,23 +1,48 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n=height.size();
-      vector<int> prefix(n);
-      prefix[0]=height[0];
-      vector<int> suffix(n);
-      suffix[n-1]=height[n-1];
-      int ans=0;
-      for(int i=1;i<n;i++){
-        prefix[i]=max(prefix[i-1],height[i]);
-      } 
-      for(int i=n-2;i>=0;i--){
-        suffix[i]=max(suffix[i+1],height[i]);
-      }
-      for(int i=0;i<n;i++){
-        if(prefix[i]>=height[i] && suffix[i]>=height[i]){
-            ans+=(min(suffix[i],prefix[i])-height[i]);
+    // vector<int> nextgreater(vector<int> &arr){
+    //     stack<pair<int,int>> st;
+    //     vector<int> ans(arr.size(),-1);
+    //     for(int i=0;i<arr.size();i++){
+    //         while(!st.empty() && st.top().first<arr[i]){
+    //             auto it=st.top();st.pop();
+    //             ans[it.second]=arr[i];
+    //         }
+    //         st.push({arr[i],i});
+    //     }
+    //     return ans;
+    // }
+    // vector<int> previousgreater(vector<int> &arr){
+    //     stack<pair<int,int>> st;
+    //     vector<int> ans(arr.size(),-1);
+    //     for(int i=arr.size()-1;i>=0;i--){
+    //         while(!st.empty() && st.top().first<arr[i]){
+    //           auto it=st.top();st.pop();
+    //           ans[it.second]=arr[i];  
+    //         }
+    //         st.push({arr[i],i});
+    //     }
+    //     return ans;
+    // }
+    int trap(vector<int>& arr) {
+    //    vector<int> ng=nextgreater(arr);
+    //    vector<int> pg=previousgreater(arr);
+       vector<int> leftMax(arr.size(),-1);
+       vector<int> rightMax(arr.size(),-1);
+       leftMax[0] = arr[0];
+       for (int i = 1; i < arr.size(); i++)
+        leftMax[i] = max(leftMax[i - 1], arr[i]);
+
+       rightMax[arr.size() - 1] = arr[arr.size() - 1];
+       for (int i = arr.size() - 2; i >= 0; i--)
+        rightMax[i] = max(rightMax[i + 1], arr[i]);
+
+       int cnt=0;
+       for(int i=0;i<arr.size();i++){
+        if(rightMax[i]!=-1 && leftMax[i]!=-1){
+            cnt+=(min(rightMax[i],leftMax[i])-arr[i]);
         }
-      }
-      return ans; 
+       }
+       return cnt;
     }
 };
