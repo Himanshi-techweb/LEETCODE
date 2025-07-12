2f3ebdad7ae41a39efbbc185abd00447eacbeb23
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-12 14:40:37 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (84.41%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,44 +1,13 @@
 class Solution {
 public:
-    string common(string a,string b){
-        int x=min(a.length(),b.length());
-        string ans="";
-        int i=0;
-        while(i<x){
-            if(a[i]==b[i]){
-                ans+=a[i];
-            }
-            else{
-                break;
-            }
-            i++;
-        }return ans;
-    }
-    string longestCommonPrefix(vector<string>& str) {
-    //   string x;
-    //   stack<string> check;
-    //   for(auto x:strs){
-    //       check.push(x);
-    //   } 
-    //   while(check.size()!=1){
-    //     string temp1=check.top();
-    //     check.pop();
-    //     string temp2=check.top();
-    //     check.pop();
-    //     string ans=common(temp1,temp2);
-    //     if(ans=="")return "";
-    //     check.push(ans);
-    //   }
-    //   return check.top();
-    string prefix=str[0];
-    for(int i=1;i<str.size();i++){
-        int j=0;
-        while(j<prefix.length() && j<str[i].length() && prefix[j]==str[i][j]){
-            j++;
+    string longestCommonPrefix(vector<string>& strs) {
+        string curr=strs[0];
+        for(int i=1;i<strs.size();i++){
+           int len=min(curr.size(),strs[i].size());
+           int j=0;
+           while(j<len && curr[j]==strs[i][j])j++;
+           curr=strs[i].substr(0,j);
         }
-        prefix=prefix.substr(0,j);
-        if(prefix=="")return "";
-    }
-    return prefix;
+        return curr;
     }
 };
