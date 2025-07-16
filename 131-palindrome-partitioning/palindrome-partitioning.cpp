ea3b62ecc11a48703a68f63b26fdb15ade0c9bad
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-16 18:48:16 +0530

    Time: 83 ms (20.58%) | Memory: 88.1 MB (20.08%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -8,23 +8,21 @@ public:
         }
         return true;
     }
-    void solve(int st,int end,string a,vector<string> &arr){
+    void solve(int st,string a,vector<string> &arr){
        if(st>=a.size()){
         ans.push_back(arr);return;
        }
-       for(int i=st;i<=end;i++){
+       for(int i=st;i<a.size();i++){
         if(ispalindrome(st,i,a)){
-            // if(st+i<a.size()){
                arr.push_back(a.substr(st,i-st+1));
-               solve(i+1,end,a,arr);
+               solve(i+1,a,arr);
                arr.pop_back();
-            // }
         }
        }
     }
     vector<vector<string>> partition(string s) {
        vector<string> arr;
-       solve(0,s.size()-1,s,arr);
+       solve(0,s,arr);
        return ans; 
     }
 };
