fb37bab272051eb202ce71d9647f9ef85dc7c736
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-16 13:36:23 +0530

    Time: 3 ms (56.15%) | Memory: 14.4 MB (42.87%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void solve(int i,int tar,vector<int> &arr,vector<int> &a){
+        if(tar==0){
+            ans.push_back(a);
+            return ;
+        }
+        if(i>=arr.size())return;
+        
+        if(arr[i]<=tar){
+           a.push_back(arr[i]);
+           solve(i,tar-arr[i],arr,a);
+           a.pop_back();
+        }
+        solve(i+1,tar,arr,a);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> a;
+        solve(0,target,candidates,a);
+        return ans;
+    }
+};
