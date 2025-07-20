a53dd35512b65fb9ba076a326a4a4fc0575dfabe
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-20 16:56:42 +0530

    Time: 11 ms (55.00%) | Memory: 9.4 MB (99.32%) - LeetSync

--- 1356-minimum-number-of-moves-to-make-palindrome/minimum-number-of-moves-to-make-palindrome.cpp
+++ 1356-minimum-number-of-moves-to-make-palindrome/minimum-number-of-moves-to-make-palindrome.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     int minMovesToMakePalindrome(string s) {
-       int l=0;int h=s.size()-1;
+       int l=0;int r=s.size()-1;
        int swaps=0;
-       while(l<=h){
-         if(s[l]==s[h]){
-            l++;h--;
-         }
-         else{
-            int k=h;
-            while(l<h && s[k]!=s[l])k--;
+       while(l<=r){
+        if(s[l]==s[r]){
+            l++;r--;
+        }
+        else{
+            int k=r;
+            while(s[k]!=s[l])k--;
             if(k==l){
                 swap(s[k],s[k+1]);
                 swaps++;
-                continue;
             }
             else{
-              for(int i=k;i<h;i++){
-                swap(s[i],s[i+1]);
-                swaps++;
-              }
+                for(;k<r;k++){
+                    swap(s[k],s[k+1]);
+                    swaps++;
+                }
             }
-            l++;h--;
-         }
-       } 
+        }
+       }
        return swaps;
     }
 };
