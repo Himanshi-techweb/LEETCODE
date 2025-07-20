e85ee69c768b87acfd79d2753405e48346fde2af
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-20 20:01:31 +0530

    Time: 38 ms (11.32%) | Memory: 97.8 MB (5.35%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -3,7 +3,6 @@ public:
     bool check(int i,int j,int k,string s1,string s2,string s3,vector<vector<vector<int>>> &dp){
         if(i>=s1.size() && j>=s2.size() && k<s3.size())return false;
         if(i>=s1.size() && j>=s2.size() && k>=s3.size())return true;
-        if(s1[i]!=s3[k] && s2[j]!=s3[k])return false;
         if(dp[i][j][k]!=-1)return dp[i][j][k];
         if(s1[i]==s3[k] && check(i+1,j,k+1,s1,s2,s3,dp)){
            return dp[i][j][k]= true;
