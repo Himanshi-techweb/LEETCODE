13082662c37cf68039e575b7d0dc638af191ccb1
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-20 20:03:15 +0530

    Time: 36 ms (12.63%) | Memory: 97.8 MB (5.35%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -4,13 +4,14 @@ public:
         if(i>=s1.size() && j>=s2.size() && k<s3.size())return false;
         if(i>=s1.size() && j>=s2.size() && k>=s3.size())return true;
         if(dp[i][j][k]!=-1)return dp[i][j][k];
-        if(s1[i]==s3[k] && check(i+1,j,k+1,s1,s2,s3,dp)){
-           return dp[i][j][k]= true;
+        bool ans=false;
+        if(s1[i]==s3[k]){
+           ans=ans|| check(i+1,j,k+1,s1,s2,s3,dp);
         }
-        if(s2[j]==s3[k] && check(i,j+1,k+1,s1,s2,s3,dp)){
-            return dp[i][j][k]= true;
+        if(s2[j]==s3[k]){
+            ans=ans||check(i,j+1,k+1,s1,s2,s3,dp);
         }
-        return dp[i][j][k]= false;
+        return dp[i][j][k]= ans;
     }
     bool isInterleave(string s1, string s2, string s3) {
         int size1=s1.size();int size2=s2.size();int size3=s3.size();
