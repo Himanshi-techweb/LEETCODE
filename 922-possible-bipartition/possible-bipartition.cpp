8310d6284fdfe4726e81bc77047ca3d305c1e28e
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-22 14:47:30 +0530

    Time: 62 ms (27.10%) | Memory: 76.5 MB (18.22%) - LeetSync

--- 922-possible-bipartition/possible-bipartition.cpp
+++ 922-possible-bipartition/possible-bipartition.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
-      vector<vector<int>> arr(n+1) ; 
-      for(auto it:dislikes){
-           arr[it[0]].push_back(it[1]);
-           arr[it[1]].push_back(it[0]);
-      }
-       vector<int> color(n+1,-1);
-       for(int i=1;i<n+1;i++){
-        if(color[i]==-1){
-          queue<int> q;
-           q.push(i);
-           color[i]=0;
-           while(!q.empty()){
-         int node=q.front();
-         q.pop();
-         for(auto i:arr[node]){
-            if(color[i]==-1){
-                color[i]=!color[node];
-                q.push(i);
-            }
-            else if(color[i]==color[node]){
-                return false;
-            }
-         }
+       vector<vector<int>> adj(n+1);
+       for(auto it:dislikes){
+         adj[it[0]].push_back(it[1]);
+         adj[it[1]].push_back(it[0]);
        }
-        }
+       vector<int> visit(n+1,0);
+       vector<int> color(n+1,-1);
+       for(int i=1;i<=n;i++){
+          if(color[i]==-1){
+            queue<int> q;
+            q.push(i);visit[i]=1;color[i]=0;
+            while(!q.empty()){
+                auto front=q.front();q.pop();
+                for(auto it:adj[front]){
+                    if(color[it]==color[front])return false;
+                    else{
+                        if(color[it]==-1){
+                            color[it]=!color[front];
+                            q.push(it);
+                        }
+                    }
+                }
+            }
+          } 
        }
        return true;
     }
