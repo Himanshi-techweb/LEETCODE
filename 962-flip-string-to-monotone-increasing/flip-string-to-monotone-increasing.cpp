179ad914c5e411de138b87fcbd779c0eec6632af
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-27 16:24:06 +0530

    Time: 216 ms (16.04%) | Memory: 90.9 MB (9.38%) - LeetSync

---
+++ 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int solve(int curr,int pre,string &s,int n,vector<vector<int>> &dp) {
+    if(curr==s.size())return 0;
+    if(dp[curr][pre+1]!=-1)return dp[curr][pre+1];
+    int flip=INT_MAX;int not_flip=INT_MAX;
+
+    int ch=s[curr]-'0';
+    if(pre==-1 || ch>=pre)not_flip=solve(curr+1,ch,s,n,dp);
+    int change=(ch==0)?1:0;
+    if(pre==-1 || change>=pre){
+        flip=1+solve(curr+1,change,s,n,dp);
+    }
+    return dp[curr][pre+1]=min(flip,not_flip);
+}
+
+    int minFlipsMonoIncr(string s) {
+
+        int n=s.size();
+        if(n==1)return 0;
+        vector<vector<int>> dp(n+1,vector<int> (3,-1));
+        return solve(0,-1,s,n,dp);
+        
+    }
+};
