1b66001e23c3503684f96b06e028a93a1170a90c
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-28 23:50:23 +0530

    Time: 8 ms (47.64%) | Memory: 127.4 MB (36.71%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
-    long long solve(int i,vector<long long>&dp,vector<vector<int>> &arr){
-        int n=arr.size();
-        if(i>=n)return 0;
+    long long solve(int i,vector<vector<int>> &arr,vector<long long> &dp){
+        if(i>=arr.size() )return 0;
         if(dp[i]!=-1)return dp[i];
-        long long include=arr[i][0]+solve(arr[i][1]+i+1,dp,arr);
-        long long exclude=0+solve(i+1,dp,arr);
-        return dp[i]=max(include,exclude);
+        long long take=arr[i][0]+solve(i+arr[i][1]+1,arr,dp);
+        long long nottake=solve(i+1,arr,dp);
+        return dp[i] =max(take,nottake);
     }
     long long mostPoints(vector<vector<int>>& questions) {
-        int n=questions.size();
-       vector<long long > dp(n,0);
-    //    //vector<vector<long long>> 
-    //    return solve(0,dp,questions);
-    for(int i=n-1;i>=0;i--){
-        long long x=questions[i][0];
-        int nexti=questions[i][1]+i+1;
-        long long include=x+(nexti<n?dp[nexti]:0);
-        long long exclude=(i+1<n?dp[i+1]:0);
-        dp[i]=max(include,exclude);
-    }
-    return dp[0];
+        vector<long long> dp(questions.size(),-1);
+       return solve(0,questions,dp); 
+
     }
 };
