499c5e62df7a11a66916bbfa56c42558aa676ac8
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-31 18:19:23 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (89.11%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
-    bool search(vector<int>& arr, int tar) {
-       int low=0;
-      int high=arr.size()-1;  
-      int ans=-1;
-      while (low <= high) {
-            int mid = low + (high-low)/2;
-
-            if (arr[mid] == tar) return true;
-            if(arr[low]==arr[mid] && arr[mid]==arr[high]){
-                low++;
-                high--;
-                continue;
-                
-            }
-            // Left half is sorted
-            if (arr[low] <= arr[mid]) {
-                if (tar >= arr[low] && tar < arr[mid]) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            }
-            // Right half is sorted
-            else {
-                if (tar > arr[mid] && tar <= arr[high]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
-            }
+    bool search(vector<int>& arr, int target) {
+       int low=0;int high=arr.size()-1;
+        while(low<=high){
+        int  mid=low+(high-low)/2;
+        if(arr[mid]==target)return true;
+        while(arr[low]==arr[mid] && arr[mid]==arr[high]){
+            low++;high--;
+            if(low>=arr.size() || high<0)return false;
         }
-        return false; 
+        if(arr[low]<=arr[mid]){
+            if(arr[low]<=target  && target<arr[mid])high=mid-1;
+            else low=mid+1;
+        }
+        else{
+            if(arr[mid]<target && target<=arr[high])low=mid+1;
+            else high=mid-1;
+        }
+       }  
+       return false;  
     }
 };
