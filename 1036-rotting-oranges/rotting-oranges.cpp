07d38ca033f337bf71677fa44425deab7e2124e3
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-08-01 18:57:10 +0530

    Time: 5 ms (9.45%) | Memory: 18.3 MB (5.21%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,41 +1,42 @@
 class Solution {
 public:
+    vector<vector<int>> rc={{-1,0},{0,-1},{1,0},{0,1}};
     int orangesRotting(vector<vector<int>>& grid) {
-       int m=grid.size();
-       int n=grid[0].size();
-       vector<vector<int>> visit(m,vector<int> (n,0));
+        int m=grid.size();int n=grid[0].size();
        int fresh=0;
-       queue<pair<int,int>>q;
-       vector<vector<int>> rc={{-1,0},{0,-1},{1,0},{0,1}};
-       for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-          if(grid[i][j]==2){
-            q.push({i,j});
-            visit[i][j]=1;
-          } 
-          else if(grid[i][j]==1)fresh++; 
+       vector<vector<int>> visit=grid;
+       queue<pair<int,int>> q;
+       int cnt=0;
+       for(int i=0;i<grid.size();i++){
+        for(int j=0;j<grid[0].size();j++){
+            visit[i][j]=0;
+            if(grid[i][j]==1)fresh++;
+            else if(grid[i][j]==2){
+              q.push({i,j});
+              visit[i][j]=1;
+            }
         }
-       }int cnt=0;
+       }
+       if(fresh==0)return 0;
        while(!q.empty() && fresh>0){
         int size=q.size();
         for(int i=0;i<size;i++){
-            int row=q.front().first;
-            int col=q.front().second;
-            q.pop();
-            for(auto it:rc){
-            int nr=row+it[0];
-            int nc=col+it[1];
+           int r=q.front().first;int c=q.front().second;
+         q.pop();
+         bool flag=false;
+         for(auto it:rc){
+            int nr=it[0]+r;int nc=it[1]+c;
             if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==1 && visit[nr][nc]==0){
-                q.push({nr,nc});
                 visit[nr][nc]=1;
-                // grid[nr][nc]=2;
+                q.push({nr,nc});
+                flag=true;
                 fresh--;
-            }
+            }  
+         }
         }
-        }
-        cnt++;
-       }
-       if(fresh>0)return -1;
-       return cnt; 
+         cnt++;
+       } 
+        if(fresh>0)return-1;
+        return cnt;
     }
 };
