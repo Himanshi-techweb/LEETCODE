f83667601ec638b357326ab77cd6e04e55caa17f
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-08-01 17:32:37 +0530

    Time: 17 ms (28.59%) | Memory: 11.6 MB (44.02%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-       unordered_map<char,int> check;
-       for(auto x:t){
-        check[x]++;
-       } int l=0;
-       int r=0;
-       int count=0;
-       int s_index=0;
-       int min_len=INT_MAX;
-       while(r<s.length() ){
-            if(check[s[r]]>0){
-                count++;
+      unordered_map<char,int> check;
+      unordered_map<char,int> require;
+      for(auto it:t)require[it]++;
+      int l=0;int r=0;int cnt=0;int len=INT_MAX;int s_i=-1;
+      int curr=0;
+      while(l<=r && r<s.size()){
+        //check for each character if it is in check map
+        check[s[r]]++;
+        if(require.count(s[r]) && check[s[r]]==require[s[r]])curr++;
+        while(curr==require.size()){
+            if(r-l+1<len){
+                len=r-l+1;s_i=l;
             }
-            check[s[r]]--;
-            r++;
-            while(count==t.size()){
-               if((r-l)<min_len){
-                min_len=r-l;
-                s_index=l;
-                }
-                check[s[l]]++;
-                if(check[s[l]]>0)count--;    
-                l++;
-            }
-            
-       }
-       if(min_len==INT_MAX){
-        return "";
-       }
-       string ans =s.substr(s_index,min_len);
-       return ans;
+            check[s[l]]--;
+            if(require.count(s[l]) && check[s[l]]<require[s[l]])curr--;
+            l++;
+        }
+        r++;
+      } 
+      if(len==INT_MAX)return "";
+      return s.substr(s_i,len); 
     }
 };
