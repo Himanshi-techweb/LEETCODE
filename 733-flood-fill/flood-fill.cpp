32ed939249741abd9dbf49f9c9f881f65451b6da
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-08-01 19:12:42 +0530

    Time: 5 ms (1.98%) | Memory: 18.7 MB (9.65%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& arr, int sr, int sc, int color) {
-     int row=arr.size();int col=arr[0].size();
-     vector<vector<int>> visit(row,vector<int> (col,0));
-     vector<vector<int>> copy=arr;
-     stack<pair<int,int>>st;
-     vector<vector<int>> rc={{-1,0},{0,1},{0,-1},{1,0}};
-     if(arr[sr][sc]==color)return arr;
-     int color2=arr[sr][sc];
-     if(arr[sr][sc]!=color){
-            st.push({sr,sc});
-            //visit[sr][sc]=1;
-            arr[sr][sc]=color;
-     }
-     while(!st.empty()){
-        pair<int,int> front=st.top();
-        st.pop();
-        for(int i=0;i<4;i++){
-        int nr=front.first+rc[i][0];
-        int nc=front.second+rc[i][1];
-        if(nr>=0 && nr<row && nc>=0 && nc<col && visit[nr][nc]==0 && arr[nr][nc]==color2){
-            visit[nr][nc]=1;
-            arr[nr][nc]=color;
-            st.push({nr,nc});
+    vector<vector<int>> rc={{-1,0},{0,-1},{1,0},{0,1}};
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int m=image.size();int n=image[0].size();
+        int orig=image[sr][sc];
+        if(orig==color)return image;
+        queue<pair<int,int>> q;
+        vector<vector<int>> visit=image;
+        q.push({sr,sc});
+        visit[sr][sc]=color;
+        while(!q.empty()){
+           int r=q.front().first;int c=q.front().second;
+           q.pop();
+           for(auto it:rc){
+              int nr=it[0]+r;int nc=it[1]+c;
+              if(nr>=0 && nr<m && nc>=0 && nc<n && image[nr][nc]==orig && visit[nr][nc]!=color){
+                  q.push({nr,nc});
+                  visit[nr][nc]=color;
+              }
+           }
         }
-        }
-     }
-     return arr;
+        return visit;
     }
 };
