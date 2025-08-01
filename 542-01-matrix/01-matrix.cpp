0402bdf3103f3de88dd490ed81e0be544430b99c
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-08-01 22:13:24 +0530

    Time: 194 ms (5.01%) | Memory: 76.4 MB (5.03%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+       int m=mat.size();int n=mat[0].size();
+       vector<vector<int>> rc={{0,-1},{0,1},{1,0},{-1,0}};
+       vector<vector<int>> dist(m,vector<int> (n,INT_MAX));
        queue<pair<pair<int,int>,int>> q;
-       vector<vector<int>> visit(mat.size(),vector<int> (mat[0].size(),0));
-       vector<vector<int>> ans=mat;
-       vector<vector<int>> rc={{0,-1},{-1,0},{0,1},{1,0}};
-       for(int i=0;i<mat.size();i++){
-        for(int j=0;j<mat[0].size();j++){
+       for(int  i=0;i<m;i++){
+        for(int j=0;j<n;j++){
             if(mat[i][j]==0){
-                q.push({{i,j},0});
-                visit[i][j]=1;
+               dist[i][j]=0;
+               q.push({{i,j},0});
             }
         }
        }
        while(!q.empty()){
-          int row=q.front().first.first;
-          int col=q.front().first.second;
-          int wt=q.front().second;
-          q.pop();
-          for(auto it:rc){
-            int nr=row+it[0];
-            int nc=col+it[1];
-            if(nr>=0 && nr<mat.size()  && nc>=0 && nc<mat[0].size()  && visit[nr][nc]==0 && mat[nr][nc]==1){
-                q.push({{nr,nc},wt+1});
-                visit[nr][nc]=1;
-                ans[nr][nc]=wt+1;
+         auto front=q.front();q.pop();
+         int r=front.first.first;int c=front.first.second;
+         int d=front.second;
+         for(auto it:rc){
+            int nr=it[0]+r;int nc=it[1]+c;
+            if(nr>=0 && nr<m && nc>=0 && nc<n && mat[nr][nc]==1 && dist[nr][nc]>1+dist[r][c]){
+                dist[nr][nc]=1+dist[r][c];
+                q.push({{nr,nc},dist[nr][nc]});
             }
-          }
+         }
        }
-       return ans;
+       return dist; 
     }
 };
