c751dd797a418c2b9c507a5a156d708a22da6664
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-08-02 23:44:49 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (35.56%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -11,25 +11,29 @@
  */
 class Solution {
 public:
+    long long len=INT_MIN;
+    //2*i +1 ==left and 2*i +2==right
     int widthOfBinaryTree(TreeNode* root) {
-       queue<pair<TreeNode*,long long>> q;
-       q.push({root,0LL});
-       int width=0;
-       while(!q.empty()){
-        int size=q.size();
-        int mini_length=q.front().second;
-        int first_ix=0;int end_ix=0;
-        for(int i=0;i<size;i++){
-            TreeNode* node=q.front().first;
-            int length=q.front().second-mini_length;
-            q.pop();
-            if(i==0) first_ix=length;
-            if(i==size-1) end_ix=length;
-            if(node->left)q.push({node->left,(long long)2*length+1}); 
-            if(node->right)q.push({node->right,(long long)2*length+2}); 
+        if(root->left==NULL && root->right==NULL)return 1;
+        queue<pair<TreeNode*,long long>> q;
+        q.push({root,0});
+        while(!q.empty()){
+            int size=q.size();
+            long long start=q.front().second;long long e=0;
+            for(long long i=0;i<size;i++){
+               auto front=q.front();TreeNode* node=front.first;int k=front.second;
+               q.pop();
+            //    if(i==0)start=front.second;
+               if(i==size-1)e=front.second;
+               if(node->left){
+                q.push({node->left,(long long)2*k+1-start});
+               }
+               if(node->right){
+                q.push({node->right,(long long)2*k+2-start});
+               }
+            }
+            len=max(len,(long long)e-start+1);
         }
-        width=max(width,end_ix-first_ix+1);
-       } 
-       return width;
+        return len;
     }
 };
