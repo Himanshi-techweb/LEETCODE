246d2ef0edce36414897554531bab5b145d949a0
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-08-02 17:48:09 +0530

    Time: 295 ms (5.99%) | Memory: 76.9 MB (5.02%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> rc={{-1,0},{0,-1},{1,0},{0,1}};
-    int minimumEffortPath(vector<vector<int>>& height) {
-      priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater <pair<int,pair<int,int>>>> pq;//(dist,row,col)
-      int m=height.size();
-      int n=height[0].size();
+    vector<vector<int>> rc={{0,-1},{-1,0},{0,1},{1,0}};
+    int minimumEffortPath(vector<vector<int>>& heights) {
+      int m=heights.size();int n=heights[0].size();
+      priority_queue<vector<int>,vector<vector<int>> ,greater<vector<int>> >q;
+      q.push({0,0,0});//height,row,col
+      int mini=INT_MAX;
       vector<vector<int>> dist(m,vector<int> (n,INT_MAX));
-      pq.push({0,{0,0}});
       dist[0][0]=0;
-      while(!pq.empty()){
-          int effort=pq.top().first;
-          int row=pq.top().second.first;
-          int col=pq.top().second.second;
-          pq.pop();
-          for(auto it:rc){
-            int nr=row+it[0];int nc=col+it[1];
-            if(nr>=0 && nr<m && nc>=0 && nc<n){
-                int neweffort=max(effort,abs(height[nr][nc]-height[row][col]));
-                if(neweffort<dist[nr][nc]){
-                    dist[nr][nc]=neweffort;
-                    pq.push({neweffort,{nr,nc}});
+      while(!q.empty()){
+        auto front=q.top();q.pop();
+        int effort=front[0];int r=front[1];int c=front[2];
+        if(r==m-1 && c==n-1)return effort;
+        for(auto it:rc){
+            int nr=it[0]+r;int nc=it[1]+c;
+            if(nr>=0 && nr<m && nc>=0 && nc<n ){
+                int neweffort=max(abs(heights[nr][nc]-heights[r][c]),effort);
+                if(dist[nr][nc]>neweffort){
+                  dist[nr][nc]=neweffort;
+                  q.push({neweffort,nr,nc});
                 }
             }
-          }
+        }
       }
-      return dist[m-1][n-1];
+      return mini; 
     }
 };
