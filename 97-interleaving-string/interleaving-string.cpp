6e86b3d373879d9537056b9bdf4746ce293a3721
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-08-08 16:58:46 +0530

    Time: 47 ms (7.79%) | Memory: 97.8 MB (5.03%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     bool check(int i,int j,int k,string s1,string s2,string s3,vector<vector<vector<int>>> &dp){
-        if(i>=s1.size() && j>=s2.size() && k<s3.size())return false;
-        if(i>=s1.size() && j>=s2.size() && k>=s3.size())return true;
-        if(dp[i][j][k]!=-1)return dp[i][j][k];
-        bool ans=false;
-        if(s1[i]==s3[k]){
-           ans=ans|| check(i+1,j,k+1,s1,s2,s3,dp);
-        }
-        if(s2[j]==s3[k]){
-            ans=ans||check(i,j+1,k+1,s1,s2,s3,dp);
-        }
-        return dp[i][j][k]= ans;
+        if(k>=s3.size())return true;
+        if(i<s2.size() && j<s3.size() &&  dp[i][j][k]!=-1)return dp[i][j][k];
+        if(s1[i]!=s3[k] && s2[j]!=s3[k])return dp[i][j][k]= false;
+        if(s1[i]==s3[k] && check(i+1,j,k+1,s1,s2,s3,dp))return dp[i][j][k]= true;
+        if(s2[j]==s3[k] &&  check(i,j+1,k+1,s1,s2,s3,dp))return dp[i][j][k]= true;
+        return dp[i][j][k]=false;
     }
     bool isInterleave(string s1, string s2, string s3) {
         int size1=s1.size();int size2=s2.size();int size3=s3.size();
