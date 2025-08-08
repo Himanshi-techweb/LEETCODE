a2144c18a8a50c789afd33a0a108bc7b35daaee5
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-08-08 13:44:55 +0530

    Time: 3 ms (73.63%) | Memory: 13.6 MB (58.46%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -4,10 +4,11 @@ public:
     bool solve(int start,string s,unordered_map<string,bool> &check,vector<int> &dp){
         if(start>=s.size())return true;
         if(dp[start]!=-1)return dp[start];
+        string str="";
         for(int i=start;i<s.size();i++){
-            string str=s.substr(start,i-start+1);
-            if(check.find(str)!=check.end()){
-                if(solve(i+1,s,check,dp))return dp[start]= true;
+            str.push_back(s[i]);
+            if(check.find(str)!=check.end() && solve(i+1,s,check,dp)){
+                return dp[start]= true;
             }
         }
         return dp[start]=false;
