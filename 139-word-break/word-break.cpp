18c4cb86a979233f3bcc5d51f82037e27a6f94ad
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-08-08 13:42:42 +0530

    Time: 19 ms (19.63%) | Memory: 18.6 MB (19.70%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    bool solve(int i,unordered_set<string> &st,string &x,vector<int> &dp){
-       if(i>=x.size())return true;
-       if(dp[i]!=-1)return dp[i];
-       for(int j=i;j<x.size();j++){
-            if(st.find(x.substr(i,j-i+1))!=st.end()){
-              if(solve(j+1,st,x,dp)) return dp[i]= true;
-              }
+    
+    bool solve(int start,string s,unordered_map<string,bool> &check,vector<int> &dp){
+        if(start>=s.size())return true;
+        if(dp[start]!=-1)return dp[start];
+        for(int i=start;i<s.size();i++){
+            string str=s.substr(start,i-start+1);
+            if(check.find(str)!=check.end()){
+                if(solve(i+1,s,check,dp))return dp[start]= true;
             }
-       
-        return dp[i]= false;
+        }
+        return dp[start]=false;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-      unordered_set<string> st;
-      for(auto it :wordDict)st.insert(it);
       vector<int> dp(s.size(),-1);
-      return solve(0,st,s,dp);
+      unordered_map<string,bool> check;
+      for(auto it:wordDict){
+        check[it]=false;
+      }
+      return solve(0,s,check,dp);
     }
 };
