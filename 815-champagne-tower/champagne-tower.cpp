cf80c86cd20e17a55e9dea3967f6b2ca6342e5b1
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-02-14 15:16:08 +0530

    Time: 11 ms (25.42%) | Memory: 17.8 MB (48.67%) - LeetSync

--- 815-champagne-tower/champagne-tower.cpp
+++ 815-champagne-tower/champagne-tower.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
-    double solve(int pour,int i,int j,vector<vector<double>> &dp){
-        if(i==0 && j==0 )return pour;
-        if(i<0 || j<0 || i<j)return 0.0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        double left=(solve(pour,i-1,j-1,dp)-1)/2.0;
-        if(left<0)left=0;
-        double right=(solve(pour,i-1,j,dp)-1)/2.0;
-        if(right<0)right=0;
-        return dp[i][j]= left+right;
-    }
     double champagneTower(int poured, int query_row, int query_glass) {
-        vector<vector<double>> dp(100+1,vector<double> (100+1,-1));
-        if(poured==0.0)return 0.0;
-        // if(poured>1e8)return 1.0;
-        if(query_row==0 && query_glass==0)return 1.0;
-        double ans= solve(poured,query_row,query_glass,dp);
-        if(ans>1.0)return 1.0;
-        return ans;
+        //in each row(i) there must be col from 1 to row (i)
+        //check for (i,j) number of glass on it
+        // if number of glass is greater than 
+        vector<vector<double>> check(query_row+1,vector<double>(query_row+1,0.0));
+        if(poured==1){
+            if(query_row==0 && query_glass==0)return 1;
+            return 0;
+        }
+        check[0][0]=(poured);
+        double left=0.0;
+        double right=0.0;
+        for(int i=1;i<=query_row;i++){
+            for(int j=0;j<=i;j++){
+                if(j==0){
+                    check[i][0]=max(0.0,(check[i-1][0]-1)/2.0);
+                }
+                else if(j==query_row){
+                    check[i][query_row]=max(0.0,(check[i-1][query_row-1]-1)/2.0);
+                }
+                else{
+                    left=max(0.0,(check[i-1][j-1]-1)/2.0);
+                    right=max(0.0,(check[i-1][j]-1)/2.0);
+                    check[i][j]=left+right;
+                }
+            }
+        }
+       return min(1.0,check[query_row][query_glass]); 
     }
 };
