826ff1504fc0563aa758b488a010abc5c1849406
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-02-18 18:21:32 +0530

    Time: 3 ms (1.12%) | Memory: 8.1 MB (5.50%) - LeetSync

---
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+       //first check first left most bit in binary representation
+    //    int x=(n&1);
+    //    int curr=0;
+    //    n=n>>1;
+    //    for(int i=1;i<32;i++){
+    //       //take xor of prev and curr bit if 1 ok if 0 then false
+    //       curr=(n&1);
+    //       if(curr^x==0)return false;
+    //       n=n>>1;
+    //    }
+    //    return true;
+     vector<int> result;
+    while(n>0){
+      int currrem=n%2;
+      if(result.size()>=1 && (result.back() ^ currrem )== 0)return false;
+      result.push_back(currrem); 
+      n=n/2;
+    }
+    for(auto it:result)cout<<it<<" ";
+    return true;
+    }
+};
