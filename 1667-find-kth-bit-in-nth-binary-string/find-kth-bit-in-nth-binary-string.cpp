72180de644776ff871f58c1c2b1661cc23dbb294
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-03-03 23:28:14 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (63.07%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    string check(string &s0){
-        //inverse 
-        string s="";
-        for(int i=s0.size()-1;i>=0;i--){
-            s=s+(s0[i]==1?'0':'1');
-        }
-        return s;
-    }
+    // string check(string &s0){
+    //     //inverse 
+    //     string s="";
+    //     for(int i=s0.size()-1;i>=0;i--){
+    //         s=s+(s0[i]==1?'0':'1');
+    //     }
+    //     return s;
+    // }
     char findKthBit(int n, int k) {
     if (n == 1) return '0'; // Base case
-    int mid = (1 << (n - 1)); // Find the middle position
+    int len=(1<<n)-1;
+    int mid = (len+1)/2; // Find the middle position
     
     if (k == mid) return '1'; // Middle bit is always '1'
     if (k < mid) return findKthBit(n - 1, k); // Search in left half
