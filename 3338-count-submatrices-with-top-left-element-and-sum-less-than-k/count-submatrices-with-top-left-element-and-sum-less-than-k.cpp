49f08bbfb79ba56570a2ea6c376bbb764842e7e7
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-03-18 14:33:47 +0530

    Time: 10 ms (82.02%) | Memory: 90.6 MB (74.72%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int countSubmatrices(vector<vector<int>>& grid, int k) {
-       int m=grid.size();
-       int n=grid[0].size();
-       vector<vector<int>> prefixsum(m,vector<int>(n,0));
+    int countSubmatrices(vector<vector<int>>& prefixsum, int k) {
+       int m=prefixsum.size();
+       int n=prefixsum[0].size();
+    //    vector<vector<int>> prefixsum(m,vector<int>(n,0));
     //    vector<int> prefixcol(n,0);
     //    vector<int> suffixsum(m,0);
-        prefixsum[0][0]=grid[0][0];
+        prefixsum[0][0]=prefixsum[0][0];
         // for(int j=1;j<n;j++){
-        //     // prefixsum[i]=grid[i]+prefix[i-1];
-        //     prefixsum[0][j]=grid[0][j];
+        //     // prefixsum[i]=prefixsum[i]+prefix[i-1];
+        //     prefixsum[0][j]=prefixsum[0][j];
         // } 
-        for(int i=0;i<m;i++){
+        for(int i=1;i<m;i++){
             for(int j=0;j<n;j++){
-                if(i==0){
-                    prefixsum[0][j]=grid[0][j];
-                }
-                else
-                prefixsum[i][j]=grid[i][j]+prefixsum[i-1][j];
+                // if(i==0){
+                //     prefixsum[0][j]=prefixsum[0][j];
+                // }
+                // else
+                // prefixsum[i][j]=prefixsum[i][j]+prefixsum[i-1][j];
+                prefixsum[i][j]=prefixsum[i][j]+prefixsum[i-1][j];
             }
         }
         int count=0;
