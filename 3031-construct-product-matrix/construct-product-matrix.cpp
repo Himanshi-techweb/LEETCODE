2872ddd3b027d80e13b7675ff43c1ff9ed76e49d
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-03-24 18:27:28 +0530

    Time: 29 ms (84.13%) | Memory: 132 MB (80.07%) - LeetSync

--- 3031-construct-product-matrix/construct-product-matrix.cpp
+++ 3031-construct-product-matrix/construct-product-matrix.cpp
@@ -5,48 +5,66 @@ public:
     vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<long long>> suffix(n,vector<long long>(m,1));
-        vector<vector<long long>> prefix(n,vector<long long>(m,1));
-        vector<long long> rowprod(n,1);
+        long long suffix=1;long long prefix=1;
+        // vector<vector<long long>> suffix(n,vector<long long>(m,1));
+        // vector<vector<long long>> prefix(n,vector<long long>(m,1));
+        // vector<long long> rowprod(n,1);
         vector<vector<int>> ans(n,vector<int>(m,0));
         //calculate prefix and suffix ok
-        for(int i=0;i<n;i++){
-            for(int j=1;j<m;j++){
-               prefix[i][j]=(prefix[i][j-1]*grid[i][j-1])%MOD;
-            }
-        }
+        // for(int i=0;i<n;i++){
+        //     for(int j=1;j<m;j++){
+        //        prefix[i][j]=(prefix[i][j-1]*grid[i][j-1])%MOD;
+        //     }
+        // }
 
-        for(int i=0;i<n;i++){
-            for(int j=m-2;j>=0;j--){
-                suffix[i][j]=(suffix[i][j+1]*grid[i][j+1])%MOD;
-            }
-        }
+        // for(int i=0;i<n;i++){
+        //     for(int j=m-2;j>=0;j--){
+        //         suffix[i][j]=(suffix[i][j+1]*grid[i][j+1])%MOD;
+        //     }
+        // }
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                rowprod[i]=(rowprod[i]*grid[i][j])%MOD;
-            }
-        }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         rowprod[i]=(rowprod[i]*grid[i][j])%MOD;
+        //     }
+        // }
          
-        vector<long long> prefixrow(n,1);
-        vector<long long> suffixrow(n,1);
-        for(int i=1;i<n;i++){
-            prefixrow[i]=(prefixrow[i-1]*rowprod[i-1])%MOD;
-        }
+        // vector<long long> prefixrow(n,1);
+        // vector<long long> suffixrow(n,1);
+        // for(int i=1;i<n;i++){
+        //     prefixrow[i]=(prefixrow[i-1]*rowprod[i-1])%MOD;
+        // }
         
-        for(int i=n-2;i>=0;i--){
-            suffixrow[i]=(suffixrow[i+1]*rowprod[i+1])%MOD;
-        }
+        // for(int i=n-2;i>=0;i--){
+        //     suffixrow[i]=(suffixrow[i+1]*rowprod[i+1])%MOD;
+        // }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //        long long currrow=(suffix[i][j]*prefix[i][j])%MOD;
+        //        long long otherrow=(prefixrow[i]*suffixrow[i])%MOD;
+        //        ans[i][j]=(currrow*otherrow)%MOD;
+        //     }
+        // }
+
+        // return ans;
+
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-               long long currrow=(suffix[i][j]*prefix[i][j])%MOD;
-               long long otherrow=(prefixrow[i]*suffixrow[i])%MOD;
-               ans[i][j]=(currrow*otherrow)%MOD;
+                ans[i][j]=(prefix);
+                prefix=(prefix*grid[i][j])%MOD;
             }
         }
 
-        return ans;
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                ans[i][j]=(ans[i][j]*suffix)%MOD;
+                suffix=(suffix*grid[i][j])%MOD;
+            }
+        }
 
+        
+        return ans;
 
     }
 };
