842a264d2605a25e9a6e70b8f4990df393421615
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-04-02 19:34:31 +0530

    Time: 611 ms (35.75%) | Memory: 275.9 MB (55.32%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
@@ -3,42 +3,42 @@ public:
     int m,n;
     vector<pair<int,int>> rc={{0,1},{1,0}};
     vector<vector<vector<int>>> dp;
-    int coin(int i,int j,vector<vector<int>> &coins,int neu){
-        if(i>=m || j>=n)return INT_MIN;
-        if(i==m-1 && j==n-1){
-            if(coins[i][j]>=0)return dp[i][j][neu]=coins[i][j];
-            else {
-                if(neu>0)return dp[i][j][neu]= max(0,coins[i][j]);
-                return dp[i][j][neu]= coins[i][j];
-            }
-        } 
-        if(dp[i][j][neu]!=-1)return dp[i][j][neu];
-        int ans=INT_MIN;
-        int next=0;
-        for(auto it:rc){
-            int nr=it.first+i;
-            int nc=it.second+j;
-            if(nr<m && nc<n){
-               next=coin(nr,nc,coins,neu);
-               if(next!=INT_MIN)
-               {if(coins[i][j]>=0)ans=max(ans,next+coins[i][j]);
-               else{
-                 ans=max(ans,coins[i][j]+next);
-                 if(neu>0){
-                    int next2=coin(nr,nc,coins,neu-1);
-                    ans=max(next2,ans);
-                 }
-               }
-               }
-            }
-        }
-        // if(dp[i][j])
-        //if negative then neu 
+    // int coin(int i,int j,vector<vector<int>> &coins,int neu){
+    //     if(i>=m || j>=n)return INT_MIN;
+    //     if(i==m-1 && j==n-1){
+    //         if(coins[i][j]>=0)return dp[i][j][neu]=coins[i][j];
+    //         else {
+    //             if(neu>0)return dp[i][j][neu]= max(0,coins[i][j]);
+    //             return dp[i][j][neu]= coins[i][j];
+    //         }
+    //     } 
+    //     if(dp[i][j][neu]!=-1)return dp[i][j][neu];
+    //     int ans=INT_MIN;
+    //     int next=0;
+    //     for(auto it:rc){
+    //         int nr=it.first+i;
+    //         int nc=it.second+j;
+    //         if(nr<m && nc<n){
+    //            next=coin(nr,nc,coins,neu);
+    //            if(next!=INT_MIN)
+    //            {if(coins[i][j]>=0)ans=max(ans,next+coins[i][j]);
+    //            else{
+    //              ans=max(ans,coins[i][j]+next);
+    //              if(neu>0){
+    //                 int next2=coin(nr,nc,coins,neu-1);
+    //                 ans=max(next2,ans);
+    //              }
+    //            }
+    //            }
+    //         }
+    //     }
+    //     // if(dp[i][j])
+    //     //if negative then neu 
        
-        return dp[i][j][neu]=ans;
+    //     return dp[i][j][neu]=ans;
 
 
-    }
+    // }
     int maximumAmount(vector<vector<int>>& coins) {
         m=coins.size();n=coins[0].size();
         dp.assign(m,vector<vector<int>>(n,vector<int>(3,INT_MIN)));
