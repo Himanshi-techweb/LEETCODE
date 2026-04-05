bca365ccdec7378a5d3954e4110907b538e3bb7c
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-04-05 11:16:43 +0530

    Time: 45 ms (27.35%) | Memory: 50.3 MB (11.97%) - LeetSync

---
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string decodeCiphertext(string encodedText, int rows) {
+        int cols=encodedText.size()/rows;
+        vector<vector<char>> arr(rows,vector<char>(cols));
+        int k=0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                arr[i][j]=encodedText[k++];
+            }
+        }
+
+
+        //traverse from 0,0 to row-1,col-1
+        string str="";
+
+        //start of each must be row 0 then increase row by 1 col by 1
+        //
+        for(int j=0;j<cols;j++){
+            int i=0;int k=j;
+            // if(j!=0 && arr[0][j]==' ')break;
+            while(i<rows && k<cols){
+                str.push_back(arr[i++][k++]);
+            }
+        }
+        int i=str.size()-1;
+        while(i>=0 && str[i]==' '){
+            if(str[i]>='a' && str[i]<='z')break;
+            i--;
+        }
+        // for(int k=0;k<i;k++)cout<<str[k];
+        
+        return str.substr(0,i+1);
+    }
+};
