a76ff40ce6885102b996b2f02108f2d5eea4daa1
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-06-25 18:05:39 +0530

    Time: 599 ms (7.28%) | Memory: 296.3 MB (24.94%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -5,7 +5,7 @@ public:
         if(i==nums.size())return 0;
         if(dp[i][prev+1]!=-1)return dp[i][prev+1];
         //check if it prev==-1 || it is larger or equal to prev index number
-        int take=INT_MIN;
+        int take=0;
         if(prev==-1 || nums[i]>nums[prev]){
             take=1+solve(i+1,i,nums);
         }
