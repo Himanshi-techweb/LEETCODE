56e38624eb54b7e4b48de85d2c69464817f07c69
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-06-27 02:28:29 +0530

    Time: 46 ms (38.00%) | Memory: 58.8 MB (95.10%) - LeetSync

--- 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.cpp
+++ 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
-    // int solve(int i,vector<int> &arr,int &diff,vector<int> &dp){
-    //    if(i>=arr.size())return 0;
-    //    if(dp[i]!=-1)return dp[i];
-    // //    if(arr[i]-arr[i-1]==diff){
-    // //     ans=max(ans,1+solve(i+1,arr,diff,dp));
-    // //    }
-    // int ans=0;
-    // for(int i=1;i<arr.size();i++){
-    //     ans=0;
-    //    if(arr[i]-arr[i-1]==diff){
-    //     ans=max(ans,1+dp[i-1]);
-    //     dp[i]=ans;
-    //    } 
+    // int solve(int i,int diff,vector<int> &arr){
+//
     // }
-    //    return dp[arr.size()-1];
-    // }
-        
-    
-    int longestSubsequence(vector<int>& arr, int diff) {
-        unordered_map<int,int> dp;
-        int ans=0;
+    int longestSubsequence(vector<int>& arr, int difference) {
+         unordered_map<int,int> dp;
+        // vector<int> dp(arr.size()+1,-1);
+        int ans=1;
         for(int i=0;i<arr.size();i++){
-            int curr=arr[i]-diff;
-            int count=0;
-            if(dp.count(curr)){
-                count=dp[curr];
+            if(dp.count(arr[i]-difference)){
+                dp[arr[i]]=1+dp[arr[i]-difference];
             }
-            dp[arr[i]]=1+count;
-            ans=max(dp[arr[i]],ans);
-        }return ans;
+            else{
+                dp[arr[i]]=1;
+            }
+            ans=max(ans,dp[arr[i]]);
+        }
+        return ans;
+
     }
 };
