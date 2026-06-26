76f7d32f76fd7afc6fe08278a90a91fbb85faa4b
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-06-27 02:00:03 +0530

    Time: 80 ms (25.12%) | Memory: 15.9 MB (37.44%) - LeetSync

--- 1680-count-all-possible-routes/count-all-possible-routes.cpp
+++ 1680-count-all-possible-routes/count-all-possible-routes.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
-    int dp[101][202];
-    const int MOD=1e9 +7;
-    int solve(int i,int fi,int fuel,vector<int> &arr,int n){
+    int s,f;
+    vector<vector<int>> dp;
+    const int MOD=1e9 + 7;
+    int solve(int i,vector<int> &arr,int fuel){
+        if(i==arr.size())return 0;
         if(fuel<0)return 0;
-        if(dp[i][fuel]!=-1)return dp[i][fuel];
-        int count=(i==fi)?1:0;
-        for(int j=0;j<n;j++){
-            if(j==i)continue;
-            int cost=abs(arr[i]-arr[j]);
-            if(fuel>=cost){
-                count=(count+solve(j,fi,fuel-cost,arr,n)%MOD)%MOD;
-            }
+        else if(fuel==0){
+            if(i==f)return 1;
+            return 0;
         }
-        return dp[i][fuel]=count;
+        if(dp[i][fuel]!=-1)return dp[i][fuel];
+        int way=0;
+         if(i==f)way+=1;
+        //reach finish location increase count and then move to next location 
+        for(int j=0;j<arr.size();j++){
+            if(j==i)continue;
+            //next location j
+            //consume fuel
+            //consider it ok
+            // if(fuel-abs(arr[i]-arr[j])>=0 && j==f)way=way+1;
+            way=(way+solve(j,arr,fuel-abs(arr[i]-arr[j])))%MOD;
+        }
+        return dp[i][fuel]=way%MOD;
     }
     int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
-        int n=locations.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(start,finish,fuel,locations,n);
+        f=finish;
+        dp.resize(locations.size()+1,vector<int>(fuel+1,-1));
+        return solve(start,locations,fuel);
     }
 };
