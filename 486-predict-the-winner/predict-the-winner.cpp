53b5ddb300acf756cd780de364b337cbd9ef7aba
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-06-29 16:43:59 +0530

    Time: 161 ms (5.19%) | Memory: 9.7 MB (69.12%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -3,18 +3,26 @@ public:
     //try it with 2 option take back or end ok
     // also check who turn //1 as alice 2 as bob
     //now to erase use start and end index 
-    int solve(int s,int e,vector<int> &arr){
+    // int solve(int s,int e,vector<int> &arr){
+    //     if(s>e)return 0;
+        
+    //     int take_s=arr[s]+min(solve(s+1,e-1,arr),solve(s+2,e,arr));
+    //     int take_e=arr[e]+min(solve(s+1,e-1,arr),solve(s,e-2,arr));
+    //     return max(take_s,take_e);
+    // }
+     int solve(int s,int e,vector<int> &arr){
         if(s>e)return 0;
         
-        int take_s=arr[s]+min(solve(s+1,e-1,arr),solve(s+2,e,arr));
-        int take_e=arr[e]+min(solve(s+1,e-1,arr),solve(s,e-2,arr));
+        int take_s=arr[s]-solve(s+1,e,arr);
+        int take_e=arr[e]-solve(s,e-1,arr);
         return max(take_s,take_e);
     }
     bool predictTheWinner(vector<int>& nums) {
 
         long long tot=accumulate(nums.begin(),nums.end(),0);
         int ans=solve(0,nums.size()-1,nums);
+        
         cout<<tot-ans<<endl;
-        return (tot-ans<=ans);
+        return ans>=0;
     }
 };
