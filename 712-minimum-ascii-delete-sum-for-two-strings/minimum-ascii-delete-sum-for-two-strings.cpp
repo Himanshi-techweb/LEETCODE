a89be6e9f0c058789c0925dfb2634a4bfdd790a9
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-06-29 18:25:15 +0530

    Time: 96 ms (5.04%) | Memory: 19.5 MB (7.31%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    long long solve(int i,int j,string &x,string &y,vector<vector<long long>> &dp){
+    vector<vector<int>> dp;
+    int solve(int i,int j,string &x,string &y){
+        if(i>=x.size() && j>=y.size())return 0;
         if(i>=x.size()){
-            long long check=0;
-            while(j<y.size()){
-                check+=(int)y[j];
-                j++;
+            int num=0;
+            for(auto k=j;k<y.size();k++){
+                num=num+y[k];
             }
-            return check;
+            return num;
         }
         if(j>=y.size()){
-            long long check=0;
-            while(i<x.size()){
-                check+=(int)x[i];
-                i++;
+            int num=0;
+            for(int k=i;k<x.size();k++){
+                num=num+x[k];
             }
-            return check;
+            return num;
         }
         if(dp[i][j]!=-1)return dp[i][j];
-        long long cost=0;
-        if(x[i]==y[j]){
-            cost=solve(i+1,j+1,x,y,dp);
-        }
-        else{
-            cost= min({(int)y[j]+solve(i,j+1,x,y,dp),(int)x[i]+solve(i+1,j,x,y,dp),(int)y[j]+(int)x[i]+solve(i+1,j+1,x,y,dp)});
-        }
-        return dp[i][j]=cost;
+        int res=0;
+        // int match=INT_MIN;int notmatch=INT_MAX;
+        //if both are equal then go to next index
+        if(x[i]==y[j])res+=solve(i+1,j+1,x,y);
+        else res+=min({y[j]+solve(i,j+1,x,y),x[i]+solve(i+1,j,x,y),x[i]+y[j]+solve(i+1,j+1,x,y)});
+        return dp[i][j]=res;
     }
     int minimumDeleteSum(string s1, string s2) {
-        int x1=s1.size();int x2=s2.size();
-        vector<vector<long long>> dp(x1+2,vector<long long> (x2+2,-1));
-        return solve(0,0,s1,s2,dp);
+        dp.resize(s1.size()+1,vector<int>(s2.size()+1,-1));
+        return solve(0,0,s1,s2);
     }
 };
