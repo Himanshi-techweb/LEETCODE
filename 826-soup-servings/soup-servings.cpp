472eee1044964a3ba35d7674facbdb9599f1fe94
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-06-29 17:12:17 +0530

    Time: 195 ms (5.10%) | Memory: 640.5 MB (6.01%) - LeetSync

--- 826-soup-servings/soup-servings.cpp
+++ 826-soup-servings/soup-servings.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> rc={{100,0},{75,25},{50,50},{25,75}};
-    unordered_map<int,unordered_map<int,double>> check;
-    double solve(int A,int B){
-        if(A<0)A=0;if(B<0)B=0;
-        if(A==0 && B!=0)return 1;
-        else if(A==0 && B==0)return 0.5;
-        else if(A!=0 && B==0)return 0;
-        if(check.find(A)!=check.end() && check[A].find(B)!=check[A].end())return check[A][B];
-        double prob=0;
-        for(auto it:rc){
-            prob=prob+0.25*solve(A-it[0],B-it[1]);
+    vector<pair<int,int>> arr={{100,0},{75,25},{50,50},{25,75}};
+    vector<vector<double>> dp;
+    double solve(int a,int b){
+        if(a<=0 && b<=0)return 0.5;
+        if(a<=0)return 1.0;
+        if(b<=0)return 0.0;
+        if(dp[a][b]!=-1)return dp[a][b];
+        double x=0.0;
+        for(auto it:arr){
+            x+=solve(a-it.first,b-it.second);
         }
-        return check[A][B]=prob;
+        return dp[a][b]=0.25 * (double)x;
+        
     }
     double soupServings(int n) {
-        if(n>=5000)return 1.0;
-       return solve(n,n); 
+        if(n>7000)return 1;
+        dp.resize(n+1,vector<double>(n+1,-1));
+        return solve(n,n);
     }
 };
