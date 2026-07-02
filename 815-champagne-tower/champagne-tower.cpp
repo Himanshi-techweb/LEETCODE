b5c6ded9d3ec40eec1ff7a37019e50c763881dbb
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-07-02 14:55:45 +0530

    Time: 8 ms (43.13%) | Memory: 30.9 MB (40.63%) - LeetSync

--- 815-champagne-tower/champagne-tower.cpp
+++ 815-champagne-tower/champagne-tower.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-    double champagneTower(int poured, int query_row, int query_glass) {
-        //in each row(i) there must be col from 1 to row (i)
-        //check for (i,j) number of glass on it
-        // if number of glass is greater than 
-        vector<vector<double>> check(query_row+1,vector<double>(query_row+1,0.0));
-        if(poured==1){
-            if(query_row==0 && query_glass==0)return 1;
-            return 0;
-        }
-        check[0][0]=(poured);
-        double left=0.0;
-        double right=0.0;
-        for(int i=1;i<=query_row;i++){
+    // double solve(int i,int j,int amount){
+    //     if(i==r && j==c)return 0;
+
+    //     //try to check for i-1,j-1 and i-1,j amount
+    //     dp[i][j]=dp[i-1]+dp[]
+    // }
+    double champagneTower(int poured, int r, int c) {
+        if(poured==0)return 0.0;
+        vector<vector<double>> dp(103,vector<double>(103,0));
+        if(r==0 && c==0 && poured>=1)return 1.0;
+        dp[0][0]=poured;
+        for(int i=1;i<=r;i++){
             for(int j=0;j<=i;j++){
-                if(j==0){
-                    check[i][0]=max(0.0,(check[i-1][0]-1)/2.0);
-                }
-                else if(j==query_row){
-                    check[i][query_row]=max(0.0,(check[i-1][query_row-1]-1)/2.0);
-                }
-                else{
-                    left=max(0.0,(check[i-1][j-1]-1)/2.0);
-                    right=max(0.0,(check[i-1][j]-1)/2.0);
-                    check[i][j]=left+right;
-                }
+                double extra1=0;
+                if(j-1>=0 && dp[i-1][j-1]>1)extra1=(dp[i-1][j-1]-1)/2.0;
+                double extra2=0;
+                if(dp[i-1][j]>1)extra2=(dp[i-1][j]-1)/2.0;
+                dp[i][j]=extra1+extra2;
             }
         }
-       return min(1.0,check[query_row][query_glass]); 
+        return (dp[r][c]>1)?1:dp[r][c];
+
     }
 };
