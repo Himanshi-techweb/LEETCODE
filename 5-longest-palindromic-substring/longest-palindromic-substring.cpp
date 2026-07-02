d920f4acdd199a52be608dffd044ad2b3e50c836
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-07-02 21:25:55 +0530

    Time: 14 ms (52.78%) | Memory: 10.6 MB (53.67%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,22 +1,46 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-       int x=s.length();
-       int max_len=0;
-       string ans;
-            auto check=[&](int l,int r){
-               while(l>=0 && r<x && s[l]==s[r]){
-              if(r-l+1>max_len){
-                max_len=max(max_len,r-l+1);
-                ans=s.substr(l,max_len);  
-              }
-              l--;
-              r++;
-              } 
-            }; 
-            for(int i=0;i<x;i++){
-                check(i,i);
-                check(i,i+1);
-            }return ans;
+    string solve1(string &s){
+        vector<int> dp(s.size(),0);int l=0;int r=0;int maxi=0;
+        for(int i=0;i<s.size();i++){
+            //traverse i to left and i to right
+            int left=i-1;int right=i+1;
+            while(left>=0 && right<s.size() && s[left]==s[right]){
+                dp[i]=right-left+1;
+                if(maxi<dp[i]){
+                l=left;
+                r=right;
+                maxi=r-l+1;
+                }            
+                left--;
+                right++;            
+            }    
         }
+        return s.substr(l,r-l+1);
+    }
+    string solve2(string s){
+        int maxi=0;
+        int l=0;int r=0;
+        for(int i=0;i<s.size();i++){
+            int left=i;
+            int right=i+1;
+            while(left>=0 && right<s.size() && s[left]==s[right]){
+                if(maxi<right-left+1){
+                    l=left;
+                    r=right;
+                    maxi=r-l+1;
+                }
+                left--;
+                right++;
+            }
+        }
+        return s.substr(l,r-l+1);
+    }
+    string longestPalindrome(string s) {
+        string str1=solve1(s);
+        string str2=solve2(s);
+        cout<<str1<<" "<<str2<<endl;
+        if(str1.size()>str2.size())return str1;
+        return str2;
+    }
 };
