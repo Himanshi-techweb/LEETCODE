34a509280b69f5f8b7d351d5ea273f719c364618
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-07-03 20:06:11 +0530

    Time: 15 ms (47.87%) | Memory: 12.3 MB (25.14%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    // int solve(int id_n,int k,int target){
-    //     if(target!=0 && id_n==0)return 0;
-    //     if(id_n!=0 && target==0)return 0;
-    //     if(target<0)return 0;
-    //     int ans=0;
-    //     for(int i=1;i<=k;i++){
-    //         ans=ans+solve(id_n-1,k,target-i);
-    //     }   
-    //     return ans;
-    // }
-    int solve(int id_n,int k,int target,vector<vector<int>>&dp){
-        if(target==0 && id_n==0)return 1;
-        if(target<0 || id_n==0)return 0;
-        if(dp[id_n][target]!=-1)return dp[id_n][target];
-        int ans=0;
-        for(int i=1;i<=k;i++){
-            ans=(ans+solve(id_n-1,k,target-i,dp))%1000000007;
-        }   
-        return dp[id_n][target]=ans;
+    int count;
+    const int MOD=1e9 +7;
+    vector<vector<int>> dp;
+    int solve(int n,int tar){
+        if(tar==0){
+            if(n==0)return 1;
+            return 0;
+        }
+        if(dp[n][tar]!=-1)return dp[n][tar];
+        int way=0;
+        for(int k=1;k<=count;k++){
+            if(tar-k>=0 && n-1>=0)way=(way+solve(n-1,tar-k)%MOD)%MOD;
+        }
+        return dp[n][tar]=way%MOD;
     }
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
-       return solve(n,k,target,dp); 
+       count=k; 
+       dp.resize(n+10,vector<int>(target+11,-1));
+       return solve(n,target);
     }
 };
