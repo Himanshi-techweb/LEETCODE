9bd0c37fce757ef0c2b0703fda888ef74173a4c2
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-07-07 14:32:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (30.20%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,26 +1,43 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-       unordered_map<char,int> check;
-       string re="";
-       priority_queue<pair<int,char>> q;
-       for(int i=0;i<s.size();i++){
-        check[s[i]]++;
-        if(check[s[i]]>(s.size()+1)/2)return "";
-       }
-       for(auto [ch,freq]:check){
-        q.push({freq,ch});
-       }
-       pair<int,char> i={0,'0'};
-       while(!q.empty()){
-          auto [cnt,ch]=q.top();q.pop();
-          re+=ch;
-          cnt--;
-          if(i.first>0){
-            q.push(i);
-          }
-          i={cnt,ch};
-       }
-       return (re.size()==s.size())?re:""; 
+        // priority_queue<pair<int,char>,vector<pair<int,char>>,greater<pair<int,char>>>q;
+        priority_queue<pair<int,char>>q;
+        priority_queue<pair<int,char>>r;
+        string ans="";
+        vector<int> freq(26,0);
+        for(int i=0;i<s.size();i++){
+            freq[s[i]-'a']++;
+        }
+        for(int i=0;i<freq.size();i++){
+            if(freq[i]!=0)q.push({freq[i],i+'a'});
+        }
+        
+        for(int i=0;i<s.size();i++){
+            while(!q.empty() && q.top().first!=freq[q.top().second-'a'])q.pop();
+            while(!q.empty() && ans.size()>0 && ans.back()==q.top().second){
+                r.push(q.top());
+                q.pop();
+            }
+            if(ans.size() ==0 || ans.back()!=q.top().second){
+                ans+=(q.top().second);
+
+                freq[q.top().second-'a']--;
+                if(freq[q.top().second-'a']>0){
+                    q.push({freq[q.top().second-'a'],q.top().second});
+                }
+                q.pop();
+            }
+
+            while(!r.empty()){
+                q.push(r.top());r.pop();
+            }
+            
+        }
+        cout<<ans<<endl;
+        return ans.size()!=s.size()?"":ans;
+
+
+
     }
 };
