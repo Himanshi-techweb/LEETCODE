3153c669cb37ddd6188fbb26b55b3b6509d6d1c0
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-07-17 19:19:16 +0530

    Time: 95 ms (17.05%) | Memory: 124.9 MB (6.29%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int,vector<int>> q;
+        unordered_map<int,int> q;
         for(auto i=0;i<nums.size();i++){
-            q[nums[i]].push_back(i);
-        }
-
-        for(auto it:q){    
-            vector<int> arr=it.second;
-            if(arr.size()==1 || (arr.size()==2 && abs(arr[1]-arr[0])>k))continue;
-            int i=0;
-            while(i<arr.size()-1){
-                if(abs(arr[i]-arr[i+1])<=k)return true;
-                i++;
+            
+            if(q.count(nums[i])){
+                if(i-q[nums[i]]<=k)return true;
             }
-
+            q[nums[i]]=i;
         }
+
+
+       
         return false;
     }
 };
