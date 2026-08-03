0721ff1b34d97464a3ee2de9f2f6a7be93e00d7d
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-08-03 18:33:56 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (97.51%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-const long long mod=1e9  +7;
+    const int MOD =1e9 +7;
+    long long findpower(int a,long long b){
+        if(b==0)return 1;
+        long long half=findpower(a,b/2);
+        long long result=(half*half)%MOD;
+        if(b%2!=0){
+            result=(a*result)%MOD;
+        }
+        return (result)%MOD;
+    }
     int countGoodNumbers(long long n) {
-      long long expeven=(n+1)/2;
-      long long expodd=n/2;
-      long long evenbase=5;long long oddbase=4;
-      //for calculation even answer;
-      long long evenans=1;
-      while(expeven>0){
-        if(expeven%2==1){
-            evenans=(evenans*evenbase)%mod;
+        //at odd index there will be 4 number possibility
+        //at even index there will be 5 number possibility
+        //if n is even then equal multiplication of n/2 (4) and n/2 (5)
+        //if n is odd then x=n-1 then x/2 (4) and x/2 (5) and then by 5
+        
+        long long half1=findpower(4,n/2);
+        long long half2=findpower(5,n/2);
+        long long result=(half1*half2)%MOD;
+        if(n%2!=0){
+            result=(result*5)%MOD;
         }
-        evenbase=(evenbase*evenbase)%mod;
-        expeven=expeven/2;
-      }
-      long long oddans=1;
-      while(expodd>0){
-        if(expodd%2==1){
-            oddans=(oddans*oddbase)%mod;
-        }
-        oddbase=(oddbase*oddbase)%mod;
-        expodd=expodd/2;
-      }
-      int ans=(evenans*oddans)%mod;
-      return ans;
+        return result%MOD;    
     }
 };
