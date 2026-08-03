908fda8c58b5dbbc193d842bd834a94458740959
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-08-03 21:10:38 +0530

    Time: 54 ms (11.61%) | Memory: 87.3 MB (28.60%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,20 +1,49 @@
 class Solution {
 public:
-    int minOperations(vector<vector<int>>& grid, int x) {
+    int minOperations(vector<vector<int>>& grid, int k) {
+
         vector<int> arr;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[i].size();j++){
-                arr.push_back(grid[i][j]);
+        for(auto x:grid){
+            for(auto y:x){
+                arr.push_back(y);
             }
-        }int check=arr[0]%x;
+        }
+        int n=arr.size();
         sort(arr.begin(),arr.end());
-        for(auto i:arr){
-            if(i%x!=check)return -1;
-        }int n=arr.size();int q=0;
-            int k=arr[n/2];
-            for(auto i:arr){
-                q+=abs((i-k))/x;
+        int bet1=arr[n/2];
+        
+        int ans=INT_MAX;
+        bool flag=true;
+        int count=0;
+        //try for bet1
+        for(int i=0;i<n;i++){
+            int x=abs(arr[i]-bet1)%k;
+            if(x!=0){
+                flag=false;
+                count=INT_MAX;
+                break;
             }
-        return q;
+            count+=((abs(arr[i]-bet1)/k));
+        }
+        if(flag)ans=min(ans,count);
+        count=0;
+        if(!flag && n>1){
+            int bet2=arr[(n/2)+1];
+            for(int i=0;i<n;i++){
+                int x=abs(arr[i]-bet2)%k;
+                if(x!=0){
+                    flag=false;
+                    count=INT_MAX;
+                    break;
+                }
+                count+=(abs(arr[i]-bet2)/k);
+            }
+        }
+        // 
+        if(ans==INT_MAX)return -1;
+        return ans;
+        
     }
+
+
 };
