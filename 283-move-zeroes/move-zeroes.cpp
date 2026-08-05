f2abf04c1173c4c0ffcbdb453a42c70847e379d4
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-08-05 17:00:41 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (98.97%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+       //get nextptr which is zero;
+       int curr=0;
+       while(curr<nums.size()){
+        if(nums[curr]==0)break;
+        curr++;
+       } 
+       //start from after it
+       int i=curr+1;
+       while(i<nums.size()){
+        if(nums[i]!=0){
+            swap(nums[i],nums[curr]);
+            //next curr will that index from curr whose value is zero
+            while(curr<=i && nums[curr]!=0)curr++;
+        }
+        i++;
+       }
+    }
+};
