a8fcef2f8a592b3ffbdc4f07b6fa3e46cad901a9
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-08-09 19:47:54 +0530

    Time: 13 ms (39.98%) | Memory: 35 MB (89.19%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    bool check(int mid,vector<int> &arr,int days){
+    bool solve(int mid,vector<int>&arr,int k){
         int cnt=0;int sum=0;
-        for(auto it:arr){
-            if(it>mid)return false;
-            if(sum+it<=mid){
-                sum=sum+it;
-            }
-            else{
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]>mid)return false;
+            if(sum+arr[i]>mid){
                 cnt++;
-                sum=it;
+                sum=arr[i];
             }
+            else sum+=arr[i];
         }
-        cnt++;
-        return cnt<=days;
-
+        if(sum<=mid)cnt++;
+        cout<<mid<<" "<<cnt<<endl;
+        return cnt<=k;
     }
-    int shipWithinDays(vector<int>& arr, int days) {
-       int low=1;
-       int high=accumulate(arr.begin(),arr.end(),0);
-       int ans=0; 
-       while(low<=high){
-        int mid=low+(high-low)/2;
-        if(check(mid,arr,days)){
-            ans=mid;
-            high=mid-1;
+    int shipWithinDays(vector<int>& weights, int days) {
+        int l=1;
+        int h=accumulate(weights.begin(),weights.end(),0);
+        int ans=0;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(solve(mid,weights,days)){
+                ans=mid;
+                h=mid-1;
+            }
+            else l=mid+1;
         }
-        else low=mid+1;
-       }
-       return ans;
+        return ans;
     }
 };
