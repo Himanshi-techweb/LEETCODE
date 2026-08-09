236cdeb2984fd3a5f4d3a0623ecd4ead7e5e4087
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-08-09 19:05:04 +0530

    Time: 22 ms (57.11%) | Memory: 70.6 MB (48.70%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
-    bool check(long long mid,int m,int k,vector<int> &Day){
-        int cnt=0;int f_cnt=0;
-        for(int i=0;i<Day.size();i++){
-           if(Day[i]<=mid){
-            f_cnt++;
-           }
-           else{
-            cnt+=(f_cnt/k);
-            f_cnt=0;
-           }
+    bool solve(int mid,int m,int k,vector<int>&arr){
+        int cnt_flower=0;
+        int bouquet=0;
+        for(int i=0;i<arr.size();i++){
+            
+            if(arr[i]>mid){
+                //discontinue all flower make cnt_flower==0
+                cnt_flower=0;
+            }
+            else cnt_flower++;
+            if(cnt_flower>=k){
+                bouquet++;
+                cnt_flower=0;
+            }
+               
         }
-        if(f_cnt>0){
-            cnt+=(f_cnt/k);
-        }
-        return cnt>=m;
+        return bouquet>=m;
+        
+        
     }
-    int minDays(vector<int>& Day, int m, int k) {
-       if(Day.size()/k<m)return -1;
-       int low=1;int high=*max_element(Day.begin(),Day.end());
-       int ans=-1;
-       while(low<=high){
-        long long mid=low+(high-low)/2;
-        if(check(mid,m,k,Day)){
-            ans=mid;
-            high=mid-1;
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int l=1;
+        int h=*max_element(bloomDay.begin(),bloomDay.end());
+        int ans=-1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(solve(mid,m,k,bloomDay)){
+                ans=mid;
+                h=mid-1;
+            }
+            else l=mid+1;
         }
-        else low=mid+1;
-       }
-       return ans; 
+        return ans;
     }
 };
