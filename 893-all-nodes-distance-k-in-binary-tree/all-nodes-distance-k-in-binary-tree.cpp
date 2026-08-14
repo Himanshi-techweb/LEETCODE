ce73de833092344b1c7bb9ae3a3bc60d8a71fa0f
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-08-14 16:34:19 +0530

    Time: 4 ms (65.53%) | Memory: 15.4 MB (49.63%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -9,59 +9,59 @@
  */
 class Solution {
 public:
-    void findroot(TreeNode* root, unordered_map<TreeNode*,TreeNode*> &arr){
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        unordered_map<TreeNode* ,TreeNode*>st;
         queue<TreeNode*> q;
         q.push(root);
+        st[root]=new TreeNode(-1);
         while(!q.empty()){
-          TreeNode* front=q.front();
-          q.pop();
-          if(front->left){
-            q.push(front->left);
-            arr[front->left]=front;
-          }
-          if(front->right){
-            q.push(front->right);
-            arr[front->right]=front;
-          }
+            auto front=q.front();q.pop();
+            if(front->left){
+                q.push(front->left);
+                st[front->left]=front;
+            }
+            if(front->right){
+                q.push(front->right);
+                st[front->right]=front;
+            }
         }
-    }
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        if(root==NULL)return {};
-       unordered_map<TreeNode*,TreeNode*> parent;
-       queue<TreeNode*> q;
-       findroot(root,parent);
-       unordered_set<TreeNode*> visi;
-       q.push(target);
-       visi.insert(target);
-       int x=0;
-       while(!q.empty()){
-        int size=q.size();
-        if(x==k)break;
-        for(int i=0;i<size;i++){
-          TreeNode* front=q.front();
-          q.pop();
-           TreeNode* el=parent[front];
-          if(front->left && visi.find(front->left)==visi.end()){
-            TreeNode* left=front->left;
-             visi.insert(left);
-             q.push(left);
-          }
-          if(front->right && visi.find(front->right)==visi.end()){
-            TreeNode* right=front->right;
-             visi.insert(right);
-             q.push(right);
-          }
-          if(front!=root && visi.find(el)==visi.end()){
-             visi.insert(el);
-             q.push(el);
-          }
-        }x++;
-       }
-       vector<int> ans;
-       while(!q.empty()){
-         ans.push_back(q.front()->val);
-         q.pop();
-       }
-       return ans;
+        q.push(target);
+        int cnt=0;
+        
+        vector<int> ans;
+        unordered_map<TreeNode*,bool> flag;
+        flag[target]=true;
+        
+        while(!q.empty() ){
+            int size=q.size();
+            if(cnt==k)break;
+            for(int i=0;i<size;i++){
+                auto front=q.front();q.pop();
+                //try it child
+                if(front->left && !flag.count(front->left)){
+                    q.push(front->left);
+                    
+                    flag[front->left]=true;
+                }
+                if(front->right && !flag.count(front->right)){
+                    q.push(front->right);
+                    
+                    flag[front->right]=true;
+                }
+                //try it parent
+                TreeNode* back=st[front];
+                if(back->val!=-1 && !flag.count(back)){
+                    q.push(back);
+                    
+                    flag[back]=true;
+                }
+            }
+            
+            cnt++;
+        }
+        while(!q.empty()){
+            ans.push_back(q.front()->val);q.pop();
+        }
+        return ans;
     }
 };
