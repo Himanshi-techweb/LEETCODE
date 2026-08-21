580a4caab3fbcb0a70246ef609c9901884d2aa3e
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-08-22 00:06:27 +0530

    Time: 363 ms (5.12%) | Memory: 29.8 MB (6.75%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    vector<int> nextGreaterElements(vector<int>& arr) {
-        vector<int> ans(arr.size(),-1);
-        int n=arr.size();
-        arr.insert(arr.end(),arr.begin(),arr.end());
-       stack<pair<int,int>> st;
-       for(int i=0;i<arr.size();i++){
-        while(!st.empty() && st.top().first<arr[i%arr.size()] ){
-            ans[st.top().second]=arr[i%arr.size()];
-            st.pop();
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int> st;
+        int n=nums[nums.size()-1];
+        //for n-1th element
+        for(int i=nums.size()-2;i>=0;i--){
+            st.push(nums[i]);
+            
         }
-        if(i<n)st.push({arr[i],i});
-       }
-       return ans;
+        cout<<st.size()<<endl;
+        int i=nums.size()-1;
+        vector<int> ans;
+        while(i>=0){
+            int x=nums[i];
+            while(!st.empty() && st.top()<=x)st.pop();
+            if(st.empty())ans.push_back(-1);
+            else ans.push_back(st.top());
+            cout<<i<<" "<<st.size()<<endl;
+            st.push(nums[i]);
+            i--;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
+    //stack
+    //   3 2
 };
