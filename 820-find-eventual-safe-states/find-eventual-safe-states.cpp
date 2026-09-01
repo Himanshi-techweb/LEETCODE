a5405267c31553043806f6f82bbb5c68c5a17eaa
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-09-01 18:47:02 +0530

    Time: 71 ms (6.56%) | Memory: 71.1 MB (5.11%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
-//anyone part of cycle is not safe node any node which lead to cycle is not safe node
-    bool dfscycle(int i,vector<vector<int>> &arr,vector<int> &vis,vector<int> &pathvis,vector<int> &check){
-        pathvis[i]=1;
-        vis[i]=1;
-        check[i]=0;
-        for(auto it:arr[i]){
-            if(!vis[it]){
-                if(dfscycle(it,arr,vis,pathvis,check)==true){
-                    check[i]=0;
-                    return true;
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int> in(n,0);
+        vector<vector<int>> revadj(n);
+        for(auto i=0;i<graph.size();i++){
+            vector<int> arr=graph[i];
+            for(auto it:arr){
+                
+                revadj[it].push_back(i);
+                in[i]++;
+            }
+        }
+        vector<int> ans;
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(in[i]==0)q.push(i);
+        }
+        // vector<int> ans;
+        
+
+        while(!q.empty()){
+            auto front=q.front();
+            ans.push_back(front);
+            q.pop();
+            for(auto it:revadj[front]){
+                in[it]--;
+                if(in[it]==0){
+                    q.push(it);
                 }
             }
-            else if(pathvis[it]){
-                check[i]=0;
-                return true;
-            }
         }
-        check[i]=1;
-        pathvis[i]=0;
-        return false;
-    }
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-      int n=graph.size();
-       vector<int> vis(n,0);
-       vector<int> pathvis(n,0);
-       vector<int> check(n,0);
-       for(int i=0;i<n;i++){
-        if(!vis[i]){
-            dfscycle(i,graph,vis,pathvis,check);
-        }
-       }
-       vector<int> ans;
-       for(int i=0;i<n;i++){
-        if(check[i]==1){
-            ans.push_back(i);
-        }
-       } 
-       return ans; 
+        sort(ans.begin(),ans.end());
+        return ans;
+        
+
     }
 };
