c1a9f92c9d0c99f0e40b60132528b98c839087d2
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-09-06 18:14:04 +0530

    Time: 46 ms (28.65%) | Memory: 19.4 MB (69.32%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,24 +1,56 @@
+class DSU {
+    public:
+    vector<int> parent,rank,component;
+    DSU(int n){
+        parent.resize(n);
+        rank.resize(n,1);
+        component.resize(n,1);
+        for(int i=0;i<n;i++)parent[i]=i;
+    }
+    int findparent(int node){
+        if(node==parent[node])return node;
+        return parent[node]=findparent(parent[node]);
+    }
+    void unite(int x,int y){
+        int px=findparent(x);
+        int py=findparent(y);
+        if(px==py)return;
+        if(rank[px]>rank[py]){
+            parent[py]=px;
+            component[px]+=component[py];
+        }
+        else if(rank[px]<rank[py]){
+            parent[px]=py;
+            component[py]+=component[px];
+        }
+        else {
+            parent[py]=px;
+            rank[px]++;
+            component[px]+=component[py];
+        }
+    }
+};
 class Solution {
 public:
-    void dfs(int i,vector<vector<int>> &stone,vector<int> &visit){
-       int r=stone[i][0];int c=stone[i][1];
-       for(int j=0;j<stone.size();j++){
-        if(!visit[j] && (stone[j][0]==r || stone[j][1]==c)){
-            visit[j]=1;
-            dfs(j,stone,visit);
-        }
-       }
-    }
     int removeStones(vector<vector<int>>& stones) {
+        DSU dsu(stones.size() );
         int n=stones.size();
-       vector<int> visit(n,0);
-       int cnt=0;
-       for(int i=0;i<n;i++){
-        if(!visit[i]){
-            cnt++;
-          dfs(i,stones,visit);
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(stones[i][0]==stones[j][0] || stones[i][1]==stones[j][1]){
+                    dsu.unite(i,j);
+                }
+            }
         }
-       } 
-       return n-cnt;
+        int ans=0;
+        unordered_set<int> st;
+        for(int i=0;i<n;i++){
+            st.insert(dsu.findparent(i));
+        }
+
+        for(auto it:st){
+            ans+=(dsu.component[it]-1);
+        }
+        return ans;
     }
 };
