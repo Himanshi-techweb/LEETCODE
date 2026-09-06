dceca8e3abd19cc89d0810cea174e4510c966fb6
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-09-06 14:30:58 +0530

    Time: 30 ms (58.87%) | Memory: 27.1 MB (52.57%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int solve(int i,int j,string x,string y,vector<vector<int>> &dp){
-        if(j<0)return 1;
-        if(i<0)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
+    vector<vector<int>> st;
+    int solve(int i,int j,string &s ,string &t){
+        if(j>=t.size())return 1;
+        if(i>=s.size())return 0;
+        if(st[i][j]!=-1)return st[i][j];
+        int ans=0;
         //match
-        int result=0;
-        if(x[i]==y[j]){
-            result+=solve(i-1,j,x,y,dp)+solve(i-1,j-1,x,y,dp);
+        if(s[i]==t[j]){
+            ans+=solve(i+1,j+1,s,t);
+                
         }
-        else{
-            result+=solve(i-1,j,x,y,dp);
-        }
-        return dp[i][j]=result;
+        //notmatch
+        ans+=solve(i+1,j,s,t);
+        return st[i][j]=ans;
     }
-    int numDistinct(string x, string y) {
-        vector<vector<int>> dp(x.size(),vector<int>(y.size(),-1));
-        return solve(x.size()-1,y.size()-1,x,y,dp);
+    int numDistinct(string s, string t) {
+        st.resize(s.size(),vector<int>(t.size(),-1));
+        return solve(0,0,s,t);
+
     }
 };
