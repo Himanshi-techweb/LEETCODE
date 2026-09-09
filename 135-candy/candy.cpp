5e005549ff27de22e983464ce484ae26cf1bf03b
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-09-09 17:04:45 +0530

    Time: 2 ms (46.99%) | Memory: 68.4 MB (34.58%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int cnt=1;
-        vector<int> arr(ratings.size(),0);
+        // vector<int> arr(ratings.size(),0);
         for(int i=1;i<ratings.size();){
             if(ratings[i]==ratings[i-1]){
                 cnt++;
-                arr[i]=cnt;
+                // arr[i]=cnt;
                 i++;
                 continue;
 
@@ -16,21 +16,21 @@ public:
             while(i<ratings.size() && ratings[i]>ratings[i-1]){
                 peak+=1;
                 cnt+=peak;
-                arr[i]=cnt;
+                // arr[i]=cnt;
                 i++;
             }
             int below=1;
             while(i<ratings.size() && ratings[i]<ratings[i-1]){
                 
                 cnt+=below;
-                arr[i]=cnt;
+                // /arr[i]=cnt;
                 below++;
                 i++;
 
             }
             if(below>peak)cnt=(cnt+below-peak);
         }
-        for(int i=0;i<arr.size();i++)cout<<arr[i]<<" ";
+        // for(int i=0;i<arr.size();i++)cout<<arr[i]<<" ";
         return cnt;
     }
 };
